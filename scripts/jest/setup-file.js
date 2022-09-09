ca7bd59201e05ceaeba5fbc5f4ad07057c1e5066
Ramesh Reddy <ramesh.reddy@harness.io>
2022-09-09 13:10:34 +0530

    fix: [PL-27934]: update uicore version, ModalDialog snapshots, mock IntersectionObserver (#11119)
    * fix: [PL-27934]: update ModalDialog snapshots, mock IntersectionObserver
    
    * update snapshot, uicore version

--- scripts/jest/setup-file.js
+++ scripts/jest/setup-file.js
@@ -78,3 +78,27 @@ window.getComputedStyle = elt => getComputedStyle(elt)
 
 jest.setTimeout(10000)
 jest.retryTimes(3)
+
+class MockIntersectionObserver {
+  constructor() {
+    this.root = null
+    this.rootMargin = ''
+    this.thresholds = []
+    this.disconnect = () => null
+    this.observe = () => null
+    this.takeRecords = () => []
+    this.unobserve = () => null
+  }
+}
+
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: MockIntersectionObserver
+})
+
+Object.defineProperty(global, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: MockIntersectionObserver
+})
