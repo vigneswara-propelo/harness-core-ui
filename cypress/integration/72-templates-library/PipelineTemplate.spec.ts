57928c2eefc7e1259f39858f1c882e5c2a82bb6d
Naman Gupta <110456765+namanharness@users.noreply.github.com>
2022-09-14 13:52:56 +0530

    fix: [CDS-39394]: adding cypress coverage for pipeline templates (#10849)
    * fix: [CDS-39394]: adding cypress coverage for pipeline templates
    
    * removed commented code
    
    * changed templateListCallAfterSelection endpoint
    
    * code refactoring

---
+++ cypress/integration/72-templates-library/PipelineTemplate.spec.ts
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+import { templatesListRoute, gitSyncEnabledCall, pipelinesRoute } from '../../support/70-pipeline/constants'
+import {
+  versionLabel,
+  pipelineTemplateName,
+  pipelineMadeFromTemplate,
+  incompletePipelineCreationResponse,
+  pipelineTemplatePublishResponse,
+  selectedPipelineTemplateResponse,
+  applyTemplateResponse,
+  selectedTemplateListFromPipeline,
+  templateListCallAfterSelectionResponse,
+  afterUseTemplateEndpointResponse,
+  afterUseTemplatePipelineTemplateNameResponse,
+  afterUseTemplateApplyTemplateResponse,
+  afterUseTemplatePipelineTemplateInputsResponse
+} from '../../support/72-templates-library/constants'
+
+describe('Pipeline Template creation and assertion', () => {
+  const templateDetailsCall =
+    '/template/api/templates/templateInputs/testPipelineTemplate?routingId=accountId&accountIdentifier=accountId&orgIdentifier=default&projectIdentifier=project1&versionLabel=v1.0&getDefaultFromOtherRepo=true'
+  const pipelineTemplatePublishCall =
+    '/template/api/templates/applyTemplates?routingId=accountId&accountIdentifier=accountId&orgIdentifier=default&projectIdentifier=project1&getDefaultFromOtherRepo=true'
+  const pipelineTemplateCreationCall =
+    '/template/api/templates?accountIdentifier=accountId&projectIdentifier=project1&orgIdentifier=default&comments='
+  const templatesListCall =
+    '/template/api/templates/list?routingId=accountId&accountIdentifier=accountId&orgIdentifier=default&projectIdentifier=project1&templateListType=Stable&searchTerm=&page=0&size=20&includeAllTemplatesAvailableAtScope=true'
+
+  const templateListCallAfterSelection =
+    '//template/api/templates/list?routingId=accountId&accountIdentifier=accountId&orgIdentifier=default&projectIdentifier=project1&module=cd&templateListType=All'
+  const applyTemplateEndpoint =
+    '/template/api/templates/applyTemplates?routingId=accountId&accountIdentifier=accountId&orgIdentifier=default&projectIdentifier=project1&getDefaultFromOtherRepo=true'
+  const afterUseTemplateEndPoint =
+    '/template/api/templates/list?accountIdentifier=accountId&orgIdentifier=default&projectIdentifier=project1&templateListType=Stable&getDefaultFromOtherRepo=true'
+  const afterUseTemplatePipelineTemplateName =
+    '/template/api/templates/testPipelineTemplate?routingId=accountId&accountIdentifier=accountId&projectIdentifier=project1&orgIdentifier=default&versionLabel=v1.0&getDefaultFromOtherRepo=true'
+  const afterUseTemplateApplyTemplateEndpoint =
+    '/template/api/templates/applyTemplates?routingId=accountId&accountIdentifier=accountId&projectIdentifier=project1&orgIdentifier=default&pipelineIdentifier=Pipeline_From_Template_Test&getDefaultFromOtherRepo=true'
+  const afterUseTemplatePipelineTemplateInputsEndpoint =
+    '/template/api/templates/templateInputs/testPipelineTemplate?routingId=accountId&accountIdentifier=accountId&projectIdentifier=project1&orgIdentifier=default&versionLabel=v1.0&getDefaultFromOtherRepo=true'
+  beforeEach(() => {
+    cy.on('uncaught:exception', () => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false
+    })
+    cy.intercept('GET', gitSyncEnabledCall, { connectivityMode: null, gitSyncEnabled: false })
+    cy.initializeRoute()
+  })
+
+  it('asserting error when creating a pipeline template', () => {
+    cy.visit(templatesListRoute, {
+      timeout: 30000
+    })
+
+    cy.intercept('POST', pipelineTemplateCreationCall, incompletePipelineCreationResponse).as(
+      'pipelineTemplateCreation'
+    )
+    cy.intercept('POST', pipelineTemplatePublishCall, pipelineTemplatePublishResponse).as('pipelineTemplatePublish')
+
+    cy.visitPageAssertion('[class*=TemplatesPage-module_templatesPageBody]')
+
+    cy.contains('span', 'New Template').click()
+    cy.get('.bp3-menu > :nth-child(3)').click() // querying "Pipeline" in "New Template" menu and clicking it
+
+    cy.contains('p', 'Create New Pipeline Template').should('be.visible')
+    cy.contains('p', 'PIPELINE').should('be.visible') //
+    //clicking "Start" without entering version label assertion
+    cy.get('button[type="submit"]').click()
+    cy.contains('span', 'Version Label is required').should('be.visible') //
+
+    cy.get('input[name="name"]').clear().type(pipelineTemplateName)
+    cy.get('input[name="versionLabel"]').clear().type(versionLabel)
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('p', pipelineTemplateName).should('be.visible') //
+    cy.contains('span', 'Unsaved changes').should('be.visible') //
+
+    cy.contains('span', 'Save').click()
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('span', 'yamlNode provided doesn not have root yaml field: pipeline').should('be.visible') //
+  })
+  it('create pipeline with pipeline template', () => {
+    cy.intercept('POST', templatesListCall, selectedTemplateListFromPipeline).as('templateListCallPipelineTemplate')
+    cy.intercept('GET', templateDetailsCall, selectedPipelineTemplateResponse).as('selectedPipelineTemplateResponse')
+    cy.intercept('POST', applyTemplateEndpoint, applyTemplateResponse).as('applyTemplateCall')
+    cy.intercept('POST', templateListCallAfterSelection, templateListCallAfterSelectionResponse).as(
+      'templateListCallAfterSelection'
+    )
+    cy.intercept('POST', afterUseTemplateEndPoint, afterUseTemplateEndpointResponse).as('afterUseTemplate')
+    cy.intercept('GET', afterUseTemplatePipelineTemplateName, afterUseTemplatePipelineTemplateNameResponse).as(
+      'afterUseTemplatePipelineTemplateName'
+    )
+    cy.intercept('POST', afterUseTemplateApplyTemplateEndpoint, afterUseTemplateApplyTemplateResponse).as(
+      'afterUseTemplateApplyTemplate'
+    )
+    cy.intercept('GET', afterUseTemplatePipelineTemplateInputsEndpoint, afterUseTemplatePipelineTemplateInputsResponse)
+
+    cy.visit(pipelinesRoute, {
+      timeout: 30000
+    })
+    cy.contains('span', 'Create a Pipeline').eq(0).should('be.visible')
+    cy.contains('span', 'Create a Pipeline').eq(0).click()
+    cy.contains('span', 'Start with Template').should('be.visible')
+    cy.contains('span', 'Start with Template').click()
+    cy.contains('span', 'Pipeline Name is a required field').should('be.visible')
+
+    cy.fillField('name', pipelineMadeFromTemplate)
+    cy.contains('span', 'Start with Template').click()
+
+    cy.contains('p', 'PIPELINE').should('be.visible')
+    cy.contains('p', 'PIPELINE').click({ force: true })
+    cy.wait(1000)
+
+    cy.get('p[data-testid="testPipelineTemplate"]').should('be.visible') //
+    cy.contains('p', 'testPipelineTemplate').should('be.visible') //
+    cy.contains('p', 'testPipelineTemplate (v1.0)').should('be.visible') //
+    cy.contains('p', 'Type').should('be.visible')
+    cy.contains('p', 'Tags').should('be.visible')
+    cy.contains('p', 'Description').should('be.visible')
+    cy.contains('p', 'Version Label').should('be.visible')
+    cy.contains('p', /^Stage:/).should('have.text', 'Stage: teststage')
+
+    cy.contains('span', 'Use Template').click()
+
+    cy.contains('div', 'Pipeline Studio').should('be.visible')
+    cy.contains('a', 'Pipeline Studio').should('be.visible')
+    cy.contains('p', /^Using Template:/).should('have.text', 'Using Template: testPipelineTemplate (v1.0)')
+    cy.contains('p', pipelineMadeFromTemplate).should('be.visible')
+    cy.contains('div', 'Unsaved changes').should('be.visible')
+    cy.contains('span', 'Run').should('be.visible')
+    cy.contains('teststage').should('be.visible')
+    cy.contains('p', /^Stage:/).should('have.text', 'Stage: teststage')
+
+    cy.contains('span', 'Save').click()
+    cy.contains(
+      'span',
+      'Invalid yaml: $.pipeline.stages[0].stage.spec.execution: is missing but it is required'
+    ).should('be.visible')
+  })
+})
---
+++ cypress/support/72-templates-library/constants.ts
@@ -0,0 +1,264 @@
+export const versionLabel = '1122'
+export const pipelineTemplateName = 'New Pipeline Template Name'
+export const pipelineMadeFromTemplate = 'Pipeline From Template Test'
+export const templateUsedForPipeline = 'testPipelineTemplate'
+
+export const incompletePipelineCreationResponse = {
+  status: 'ERROR',
+  code: 'TEMPLATE_EXCEPTION',
+  message: 'yamlNode provided doesn not have root yaml field: pipeline',
+  correlationId: '32e010a4-a01f-4595-b8c1-f7c2d79fb4c6',
+  detailedMessage: null,
+  responseMessages: [
+    {
+      code: 'TEMPLATE_EXCEPTION',
+      level: 'ERROR',
+      message: 'yamlNode provided doesn not have root yaml field: pipeline',
+      exception: null,
+      failureTypes: []
+    }
+  ],
+  metadata: null
+}
+
+export const pipelineTemplatePublishResponse = {
+  status: 'SUCCESS',
+  data: {
+    mergedPipelineYaml:
+      'name: "New Pipeline Template Name"\nidentifier: "new_pipeline_template_name"\nversionLabel: "1122"\ntype: "Pipeline"\nprojectIdentifier: "project1"\norgIdentifier: "default"\ntags: {}\n',
+    templateReferenceSummaries: []
+  },
+  metaData: null,
+  correlationId: '8b05db42-9f08-4850-87bb-e5c8c4ed967e'
+}
+export const selectedPipelineTemplateResponse = {
+  status: 'SUCCESS',
+  data: 'stages:\n- stage:\n    identifier: "teststage"\n    type: "Deployment"\n    spec:\n      service:\n        serviceRef: "<+input>"\n        serviceInputs: "<+input>"\n      environment:\n        environmentRef: "<+input>"\n        environmentInputs: "<+input>"\n        serviceOverrideInputs: "<+input>"\n        infrastructureDefinitions: "<+input>"\n',
+  metaData: null,
+  correlationId: 'abac43d0-0d82-4500-a61a-ba234cff6b6f'
+}
+export const applyTemplateResponse = {
+  status: 'SUCCESS',
+  data: {
+    mergedPipelineYaml:
+      'name: ${templateUsedForPipeline}\nidentifier: ${templateUsedForPipeline}\nversionLabel: "v1.0"\ntype: "Pipeline"\nprojectIdentifier: "TemplateTestSanity3"\norgIdentifier: "Pipelines_UI_Organisation"\ntags: {}\nspec:\n  stages:\n  - stage:\n      name: "teststage"\n      identifier: "teststage"\n      description: ""\n      type: "Deployment"\n      spec:\n        deploymentType: "Kubernetes"\n        service:\n          serviceRef: "<+input>"\n          serviceInputs: "<+input>"\n        environment:\n          environmentRef: "<+input>"\n          deployToAll: false\n          environmentInputs: "<+input>"\n          serviceOverrideInputs: "<+input>"\n          infrastructureDefinitions: "<+input>"\n        execution:\n          steps:\n          - step:\n              name: "Rollout Deployment"\n              identifier: "rolloutDeployment"\n              type: "K8sRollingDeploy"\n              timeout: "10m"\n              spec:\n                skipDryRun: false\n          rollbackSteps:\n          - step:\n              name: "Rollback Rollout Deployment"\n              identifier: "rollbackRolloutDeployment"\n              type: "K8sRollingRollback"\n              timeout: "10m"\n              spec: {}\n      tags: {}\n      failureStrategies:\n      - onFailure:\n          errors:\n          - "AllErrors"\n          action:\n            type: "StageRollback"\n',
+    templateReferenceSummaries: []
+  },
+  metaData: null,
+  correlationId: '416b502d-11bb-461b-9706-7aa40d95016f'
+}
+
+export const selectedTemplateListFromPipeline = {
+  status: 'SUCCESS',
+  data: {
+    content: [
+      {
+        accountId: 'accountId',
+        orgIdentifier: 'default',
+        projectIdentifier: 'project1',
+        identifier: templateUsedForPipeline,
+        name: templateUsedForPipeline,
+        description: '',
+        tags: {},
+        yaml: 'template:\n  name: ${templateUsedForPipeline}\n  identifier: ${templateUsedForPipeline}\n  versionLabel: v1.0\n  type: Pipeline\n  projectIdentifier: project1\n  orgIdentifier: default\n  tags: {}\n  spec:\n    stages:\n      - stage:\n          name: testStage\n          identifier: testStage\n          description: ""\n          type: Deployment\n          spec:\n            serviceConfig:\n              serviceRef: service_1\n              serviceDefinition:\n                spec:\n                  variables: []\n                type: Kubernetes\n            infrastructure:\n              infrastructureDefinition:\n                type: KubernetesDirect\n                spec:\n                  connectorRef: <+input>\n                  namespace: <+input>\n                  releaseName: release-<+INFRA_KEY>\n              allowSimultaneousDeployments: false\n              environmentRef: <+input>\n            execution:\n              steps:\n                - step:\n                    name: Rollout Deployment\n                    identifier: rolloutDeployment\n                    type: K8sRollingDeploy\n                    timeout: 10m\n                    spec:\n                      skipDryRun: false\n              rollbackSteps:\n                - step:\n                    name: Rollback Rollout Deployment\n                    identifier: rollbackRolloutDeployment\n                    type: K8sRollingRollback\n                    timeout: 10m\n                    spec: {}\n          tags: {}\n          failureStrategies:\n            - onFailure:\n                errors:\n                  - AllErrors\n                action:\n                  type: StageRollback\n',
+        versionLabel: 'v1.0',
+        templateEntityType: 'Pipeline',
+        templateScope: 'project',
+        version: 0,
+        gitDetails: {
+          objectId: null,
+          branch: null,
+          repoIdentifier: null,
+          rootFolder: null,
+          filePath: null,
+          repoName: null,
+          commitId: null,
+          fileUrl: null,
+          repoUrl: null
+        },
+        entityValidityDetails: {
+          valid: true,
+          invalidYaml: null
+        },
+        lastUpdatedAt: 1661243271130,
+        createdAt: 1661243271130,
+        stableTemplate: true
+      }
+    ],
+    pageable: {
+      sort: {
+        unsorted: false,
+        sorted: true,
+        empty: false
+      },
+      pageNumber: 0,
+      pageSize: 25,
+      offset: 0,
+      paged: true,
+      unpaged: false
+    },
+    totalPages: 1,
+    totalElements: 1,
+    last: true,
+    sort: {
+      unsorted: false,
+      sorted: true,
+      empty: false
+    },
+    number: 0,
+    first: true,
+    numberOfElements: 1,
+    size: 25,
+    empty: false
+  },
+  metaData: null,
+  correlationId: 'cefe0619-9a2c-486d-ae5a-fa1f033a75e1'
+}
+export const templateListCallAfterSelectionResponse = {
+  status: 'SUCCESS',
+  data: {
+    content: [
+      {
+        accountId: 'accountId',
+        orgIdentifier: 'default',
+        projectIdentifier: 'project1',
+        identifier: templateUsedForPipeline,
+        name: templateUsedForPipeline,
+        description: '',
+        tags: {},
+        yaml: 'template:\n  name: ${templateUsedForPipeline}\n  identifier: ${templateUsedForPipeline}\n  versionLabel: v1.0\n  type: Pipeline\n  projectIdentifier: TemplateTestSanity3\n  orgIdentifier: Pipelines_UI_Organisation\n  tags: {}\n  spec:\n    stages:\n      - stage:\n          name: teststage\n          identifier: teststage\n          description: ""\n          type: Deployment\n          spec:\n            deploymentType: Kubernetes\n            service:\n              serviceRef: <+input>\n              serviceInputs: <+input>\n            environment:\n              environmentRef: <+input>\n              deployToAll: false\n              environmentInputs: <+input>\n              serviceOverrideInputs: <+input>\n              infrastructureDefinitions: <+input>\n            execution:\n              steps:\n                - step:\n                    name: Rollout Deployment\n                    identifier: rolloutDeployment\n                    type: K8sRollingDeploy\n                    timeout: 10m\n                    spec:\n                      skipDryRun: false\n              rollbackSteps:\n                - step:\n                    name: Rollback Rollout Deployment\n                    identifier: rollbackRolloutDeployment\n                    type: K8sRollingRollback\n                    timeout: 10m\n                    spec: {}\n          tags: {}\n          failureStrategies:\n            - onFailure:\n                errors:\n                  - AllErrors\n                action:\n                  type: StageRollback\n',
+        versionLabel: 'v1.0',
+        templateEntityType: 'Pipeline',
+        templateScope: 'project',
+        version: 0,
+        gitDetails: {
+          objectId: null,
+          branch: null,
+          repoIdentifier: null,
+          rootFolder: null,
+          filePath: null,
+          repoName: null,
+          commitId: null,
+          fileUrl: null,
+          repoUrl: null
+        },
+        entityValidityDetails: {
+          valid: true,
+          invalidYaml: null
+        },
+        lastUpdatedAt: 1661406218981,
+        createdAt: 1661406218981,
+        stableTemplate: true
+      }
+    ],
+    pageable: {
+      sort: {
+        sorted: true,
+        unsorted: false,
+        empty: false
+      },
+      pageSize: 25,
+      pageNumber: 0,
+      offset: 0,
+      paged: true,
+      unpaged: false
+    },
+    totalPages: 1,
+    totalElements: 1,
+    last: true,
+    sort: {
+      sorted: true,
+      unsorted: false,
+      empty: false
+    },
+    number: 0,
+    first: true,
+    numberOfElements: 1,
+    size: 25,
+    empty: false
+  },
+  metaData: null,
+  correlationId: '1c6439a1-d4d8-46f2-af04-d658ede277ff'
+}
+
+export const afterUseTemplateEndpointResponse = {
+  status: 'SUCCESS',
+  data: {
+    mergedPipelineYaml:
+      'pipeline:\n  name: ${pipelineMadeFromTemplate}\n  identifier: ${pipelineMadeFromTemplate}\n  stages:\n  - stage:\n      identifier: "teststage"\n      type: "Deployment"\n      name: "teststage"\n      description: ""\n      spec:\n        serviceConfig:\n          serviceRef: "servicetest"\n          serviceDefinition:\n            type: "ServerlessAwsLambda"\n            spec:\n              variables: []\n        execution:\n          steps:\n          - step:\n              identifier: "rolloutDeployment"\n              type: "K8sRollingDeploy"\n              name: "Rollout Deployment"\n              timeout: "10m"\n              spec:\n                skipDryRun: false\n          rollbackSteps:\n          - step:\n              identifier: "rollbackRolloutDeployment"\n              type: "K8sRollingRollback"\n              name: "Rollback Rollout Deployment"\n              timeout: "10m"\n              spec: {}\n        infrastructure:\n          infrastructureDefinition:\n            type: "ServerlessAwsLambda"\n          allowSimultaneousDeployments: false\n      tags: {}\n      failureStrategies:\n      - onFailure:\n          errors:\n          - "AllErrors"\n          action:\n            type: "StageRollback"\n  tags: {}\n  projectIdentifier: "TemplateTestSanity3"\n  orgIdentifier: "Pipelines_UI_Organisation"\n',
+    templateReferenceSummaries: [
+      {
+        fqn: 'pipeline',
+        templateIdentifier: templateUsedForPipeline,
+        versionLabel: 'v1',
+        scope: 'project',
+        stableTemplate: false,
+        moduleInfo: []
+      }
+    ]
+  },
+  metaData: null,
+  correlationId: 'af36bd52-f04b-48a0-a699-f0c9bdaaf398'
+}
+export const afterUseTemplatePipelineTemplateNameResponse = {
+  status: 'SUCCESS',
+  data: {
+    accountId: 'accountId',
+    orgIdentifier: 'default',
+    projectIdentifier: 'project1',
+    identifier: templateUsedForPipeline,
+    name: templateUsedForPipeline,
+    description: '',
+    tags: {},
+    yaml: 'template:\n  name: ${templateUsedForPipeline}\n  identifier: ${templateUsedForPipeline}\n  versionLabel: v1\n  type: Pipeline\n  projectIdentifier: TemplateTestSanity3\n  orgIdentifier: Pipelines_UI_Organisation\n  tags: {}\n  spec:\n    stages:\n      - stage:\n          name: teststage\n          identifier: teststage\n          description: ""\n          type: Deployment\n          spec:\n            serviceConfig:\n              serviceRef: servicetest\n              serviceDefinition:\n                spec:\n                  variables: []\n                type: ServerlessAwsLambda\n            execution:\n              steps:\n                - step:\n                    name: Rollout Deployment\n                    identifier: rolloutDeployment\n                    type: K8sRollingDeploy\n                    timeout: 10m\n                    spec:\n                      skipDryRun: false\n              rollbackSteps:\n                - step:\n                    name: Rollback Rollout Deployment\n                    identifier: rollbackRolloutDeployment\n                    type: K8sRollingRollback\n                    timeout: 10m\n                    spec: {}\n            infrastructure:\n              environmentRef: <+input>\n              infrastructureDefinition:\n                type: ServerlessAwsLambda\n                spec:\n                  connectorRef: <+input>\n                  stage: <+input>\n                  region: <+input>\n              allowSimultaneousDeployments: false\n          tags: {}\n          failureStrategies:\n            - onFailure:\n                errors:\n                  - AllErrors\n                action:\n                  type: StageRollback\n',
+    versionLabel: 'v1',
+    templateEntityType: 'Pipeline',
+    templateScope: 'project',
+    version: 0,
+    gitDetails: {
+      objectId: null,
+      branch: null,
+      repoIdentifier: null,
+      rootFolder: null,
+      filePath: null,
+      repoName: null,
+      commitId: null,
+      fileUrl: null,
+      repoUrl: null
+    },
+    entityValidityDetails: {
+      valid: true,
+      invalidYaml: null
+    },
+    lastUpdatedAt: 1661168203483,
+    storeType: 'INLINE',
+    stableTemplate: true
+  },
+  metaData: null,
+  correlationId: 'e204520d-ac73-4148-856f-e3e913d2fd3e'
+}
+export const afterUseTemplateApplyTemplateResponse = {
+  status: 'SUCCESS',
+  data: {
+    mergedPipelineYaml:
+      'pipeline:\n  name: ${pipelineMadeFromTemplate}\n  identifier: ${pipelineMadeFromTemplate}\n  stages:\n  - stage:\n      identifier: "teststage"\n      type: "Deployment"\n      name: "teststage"\n      description: ""\n      spec:\n        serviceConfig:\n          serviceRef: "servicetest"\n          serviceDefinition:\n            type: "ServerlessAwsLambda"\n            spec:\n              variables: []\n        execution:\n          steps:\n          - step:\n              identifier: "rolloutDeployment"\n              type: "K8sRollingDeploy"\n              name: "Rollout Deployment"\n              timeout: "10m"\n              spec:\n                skipDryRun: false\n          rollbackSteps:\n          - step:\n              identifier: "rollbackRolloutDeployment"\n              type: "K8sRollingRollback"\n              name: "Rollback Rollout Deployment"\n              timeout: "10m"\n              spec: {}\n        infrastructure:\n          infrastructureDefinition:\n            type: "ServerlessAwsLambda"\n          allowSimultaneousDeployments: false\n      tags: {}\n      failureStrategies:\n      - onFailure:\n          errors:\n          - "AllErrors"\n          action:\n            type: "StageRollback"\n  tags: {}\n  projectIdentifier: "TemplateTestSanity3"\n  orgIdentifier: "Pipelines_UI_Organisation"\n',
+    templateReferenceSummaries: [
+      {
+        fqn: 'pipeline',
+        templateIdentifier: templateUsedForPipeline,
+        versionLabel: 'v1.0',
+        scope: 'project',
+        stableTemplate: false,
+        moduleInfo: []
+      }
+    ]
+  },
+  metaData: null,
+  correlationId: 'af36bd52-f04b-48a0-a699-f0c9bdaaf398'
+}
+export const afterUseTemplatePipelineTemplateInputsResponse = {
+  status: 'SUCCESS',
+  data: 'stages:\n- stage:\n    identifier: "teststage"\n    type: "Deployment"\n    spec:\n      infrastructure:\n        environmentRef: "<+input>"\n        infrastructureDefinition:\n          type: "ServerlessAwsLambda"\n          spec:\n            connectorRef: "<+input>"\n            stage: "<+input>"\n            region: "<+input>"\n',
+  metaData: null,
+  correlationId: 'e097de1b-9135-4a16-82ef-344d8049c4fa'
+}
