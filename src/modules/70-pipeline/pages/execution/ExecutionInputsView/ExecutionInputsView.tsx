fe00ff2fe151fbeaf28e70815d58b2a438df5b27
Uday Vunnam <20707504+udayvunnam@users.noreply.github.com>
2022-09-30 13:07:28 +0530

    Revert "feat: [PIE-5379]: move useGetInputsetYaml to common component, open modal first and show loader on it (#11530)" (#11568)
    This reverts commit b3fffb761109463154210574961597c446b97d19.

--- src/modules/70-pipeline/pages/execution/ExecutionInputsView/ExecutionInputsView.tsx
+++ src/modules/70-pipeline/pages/execution/ExecutionInputsView/ExecutionInputsView.tsx
@@ -5,13 +5,17 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+
 import type { ExecutionPathProps, PipelineType } from '@common/interfaces/RouteInterfaces'
+import { useGetInputsetYamlV2 } from 'services/pipeline-ng'
 import { useAppStore } from 'framework/AppStore/AppStoreContext'
+import { PageSpinner } from '@common/components'
 import type { StoreType } from '@common/constants/GitSyncTypes'
-import { RunPipelineFormWithInputSetData } from '@pipeline/components/RunPipelineModal/RunPipelineForm'
+import { RunPipelineForm } from '@pipeline/components/RunPipelineModal/RunPipelineForm'
 import type { ResponseJsonNode } from 'services/cd-ng'
+
 import { useExecutionContext } from '@pipeline/context/ExecutionContext'
 import css from './ExecutionInputsView.module.scss'
 
@@ -27,22 +31,54 @@ export default function ExecutionInputsView(props: ExecutionInputsViewInterface)
   const isGitSyncEnabled = isGitSyncEnabledForProject && !gitSyncEnabledOnlyForFF
 
   const { pipelineExecutionDetail } = useExecutionContext()
+
+  const { data, loading } = useGetInputsetYamlV2({
+    planExecutionId: executionIdentifier,
+    queryParams: {
+      orgIdentifier,
+      resolveExpressions: true,
+      projectIdentifier,
+      accountIdentifier: accountId
+    },
+    requestOptions: {
+      headers: {
+        'content-type': 'application/yaml'
+      }
+    }
+  })
+
   const storeMetadata = {
     connectorRef: pipelineExecutionDetail?.pipelineExecutionSummary?.connectorRef,
     branch: pipelineExecutionDetail?.pipelineExecutionSummary?.gitDetails?.branch,
     repoName: pipelineExecutionDetail?.pipelineExecutionSummary?.gitDetails?.repoName
   }
 
+  const [inputSetYaml, setInputSetYaml] = useState('')
+  const [inputSetTemplateYaml, setInputSetTemplateYaml] = useState('')
+  useEffect(() => {
+    // Won't actually render out RunPipelineForm
+    /* istanbul ignore else */ if (data?.data?.inputSetYaml) {
+      setInputSetYaml(data.data?.inputSetYaml)
+    }
+    /* istanbul ignore else */ if (data?.data?.inputSetTemplateYaml) {
+      setInputSetTemplateYaml(data.data.inputSetTemplateYaml)
+    }
+  }, [data])
+
+  if (loading) {
+    return <PageSpinner />
+  }
+
   return (
     <div className={css.main}>
-      <RunPipelineFormWithInputSetData
+      <RunPipelineForm
         pipelineIdentifier={pipelineIdentifier}
         orgIdentifier={orgIdentifier}
         projectIdentifier={projectIdentifier}
         accountId={accountId}
         module={module}
         source={source}
-        executionIdentifier={executionIdentifier}
+        inputSetYAML={inputSetYaml || ''}
         executionView
         branch={pipelineExecutionDetail?.pipelineExecutionSummary?.gitDetails?.branch}
         repoIdentifier={
@@ -52,6 +88,7 @@ export default function ExecutionInputsView(props: ExecutionInputsViewInterface)
         }
         connectorRef={pipelineExecutionDetail?.pipelineExecutionSummary?.connectorRef}
         mockData={props.mockData}
+        executionInputSetTemplateYaml={inputSetTemplateYaml}
         storeType={pipelineExecutionDetail?.pipelineExecutionSummary?.storeType as StoreType}
         storeMetadata={storeMetadata}
       />
