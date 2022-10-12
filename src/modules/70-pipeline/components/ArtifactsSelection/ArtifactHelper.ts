a461a09325fadcee76b46f0b1d4028a692d6776f
litvinovstas <102949579+litvinovstas@users.noreply.github.com>
2022-10-12 16:08:37 +0300

    feat: [CDS-43647]:nexus2 (#11745)

--- src/modules/70-pipeline/components/ArtifactsSelection/ArtifactHelper.ts
+++ src/modules/70-pipeline/components/ArtifactsSelection/ArtifactHelper.ts
@@ -154,14 +154,16 @@ export const allowedArtifactTypes: Record<ServiceDefinition['type'], Array<Artif
     ENABLED_ARTIFACT_TYPES.Jenkins,
     ENABLED_ARTIFACT_TYPES.CustomArtifact,
     ENABLED_ARTIFACT_TYPES.Nexus3Registry,
-    ENABLED_ARTIFACT_TYPES.AmazonS3
+    ENABLED_ARTIFACT_TYPES.AmazonS3,
+    ENABLED_ARTIFACT_TYPES.Nexus2Registry
   ],
   WinRm: [
     ENABLED_ARTIFACT_TYPES.ArtifactoryRegistry,
     ENABLED_ARTIFACT_TYPES.Jenkins,
     ENABLED_ARTIFACT_TYPES.CustomArtifact,
     ENABLED_ARTIFACT_TYPES.Nexus3Registry,
-    ENABLED_ARTIFACT_TYPES.AmazonS3
+    ENABLED_ARTIFACT_TYPES.AmazonS3,
+    ENABLED_ARTIFACT_TYPES.Nexus2Registry
   ],
   AzureWebApp: [
     ENABLED_ARTIFACT_TYPES.DockerRegistry,
