8945b58737a283d9115fcaf54412b273a14f0a9c
Avinash Madhwani <avinash.madhwani@harness.io>
2022-10-20 17:15:52 +0530

    feat: [PL-29190]: Removed hard code scope from connector reference field (#12005)

--- src/modules/35-connectors/components/ConnectorReferenceField/ConnectorReferenceField.tsx
+++ src/modules/35-connectors/components/ConnectorReferenceField/ConnectorReferenceField.tsx
@@ -64,7 +64,10 @@ import { getReadableDateTime } from '@common/utils/dateUtils'
 import { Connectors } from '@connectors/constants'
 import { LinkifyText } from '@common/components/LinkifyText/LinkifyText'
 import RbacButton from '@rbac/components/Button/Button'
-import type { ScopeAndIdentifier } from '@common/components/MultiSelectEntityReference/MultiSelectEntityReference'
+import {
+  getScopeFromDTO,
+  ScopeAndIdentifier
+} from '@common/components/MultiSelectEntityReference/MultiSelectEntityReference'
 import ConnectorsEmptyState from './connectors-no-data.png'
 import css from './ConnectorReferenceField.module.scss'
 
@@ -643,7 +646,7 @@ export const ConnectorReferenceField: React.FC<ConnectorReferenceFieldProps> = p
     onSuccess: (data?: ConnectorConfigDTO) => {
       if (data) {
         setIsConnectorEdited(true)
-        props.onChange?.({ ...data.connector, status: data.status }, Scope.PROJECT)
+        props.onChange?.({ ...data.connector, status: data.status }, getScopeFromDTO(data.connector))
         setInlineSelection({
           selected: true,
           inlineModalClosed: false
@@ -662,7 +665,7 @@ export const ConnectorReferenceField: React.FC<ConnectorReferenceFieldProps> = p
     onSuccess: (data?: ConnectorConfigDTO) => {
       if (data) {
         setIsConnectorEdited(true)
-        props.onChange?.({ ...data.connector, status: data.status }, Scope.PROJECT)
+        props.onChange?.({ ...data.connector, status: data.status }, getScopeFromDTO(data.connector))
         setInlineSelection({
           selected: true,
           inlineModalClosed: false
