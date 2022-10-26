0169971fefd825acd5c5cc7737e453f19839e8a1
Ishant Mehta <43949150+ishantmehtaharness@users.noreply.github.com>
2022-10-27 01:40:35 +0530

    feat: [CDS-45142]: Jest test cases for Config Section (#12068)
    * feat: [CDS-45142]: Jest test cases for Config Section
    
    * feat: [CDS-45142]: Adds tests for Freeze Config Section

---
+++ src/modules/72-freeze-windows/components/__tests__/FreezeWindowStudioConfigSection.test.tsx
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+import React from 'react'
+import { render, waitFor, act } from '@testing-library/react'
+import userEvent, { TargetElement } from '@testing-library/user-event'
+import { TestWrapper } from '@common/utils/testUtils'
+import { fillAtForm, InputTypes } from '@common/utils/JestFormHelper'
+import { getOrganizationAggregateDTOListMockData } from '@projects-orgs/pages/organizations/__tests__/OrganizationsMockData'
+import serviceData from '@common/modals/HarnessServiceModal/__tests__/serviceMock'
+import { FreezeStudioWrapper } from '@freeze-windows/components/FreezeStudioWrapper'
+
+export const accountId = 'accountId'
+export const projectIdentifier = 'project1'
+export const orgIdentifier = 'default'
+
+jest.mock('services/cd-ng', () => ({
+  useGetOrganizationAggregateDTOList: jest.fn().mockImplementation(() => {
+    return { ...getOrganizationAggregateDTOListMockData, refetch: jest.fn(), error: null }
+  }),
+  useGetFreeze: jest.fn().mockImplementation(() => ({})),
+  useCreateFreeze: jest.fn().mockImplementation(() => ({})),
+  useUpdateFreeze: jest.fn().mockImplementation(() => ({})),
+  useGetProjectList: jest.fn().mockImplementation(() => ({})),
+  useGetServiceList: jest.fn().mockImplementation(() => ({ loading: false, data: serviceData, refetch: jest.fn() }))
+}))
+
+describe('Freeze Window Studio Config Section', () => {
+  test('it should render Config section in create mode - PROJECT LEVEL', async () => {
+    const { container, getByText } = render(
+      <TestWrapper
+        path="/account/:accountId/:module/orgs/:orgIdentifier/projects/:projectIdentifier/setup/freeze-window-studio/window/:windowIdentifier/"
+        pathParams={{ projectIdentifier, orgIdentifier, accountId, module: 'cd', windowIdentifier: '-1' }}
+      >
+        <FreezeStudioWrapper />
+      </TestWrapper>
+    )
+
+    fillAtForm([
+      {
+        container,
+        type: InputTypes.TEXTFIELD,
+        fieldId: 'name',
+        value: 'dummyname'
+      }
+    ])
+
+    const configTab = getByText('freezeWindows.freezeStudio.freezeConfiguration')
+    expect(configTab).toBeDefined()
+    userEvent.click(configTab)
+    await waitFor(() => {
+      expect(getByText('Add rule')).toBeInTheDocument()
+    })
+    expect(document.getElementById('bp3-tab-panel_freezeWindowStudio_FREEZE_CONFIG')).toMatchSnapshot(
+      'freeze config initial section rendering'
+    )
+
+    // Click on Add rule
+    const addRule = getByText('Add rule')
+    userEvent.click(addRule)
+    await waitFor(() => {
+      expect(getByText('envType')).toBeInTheDocument()
+    })
+    expect(document.getElementsByClassName('configFormContainer')[0]).toMatchSnapshot('Add Rule form snapshot')
+
+    const inputEl = document.querySelector('input[name="entity[0].name"]')
+
+    // Fields should be renderer
+    expect(inputEl).toBeDefined()
+    expect(getByText('services')).toBeDefined()
+    expect(getByText('envType')).toBeDefined()
+
+    userEvent.type(inputEl as TargetElement, 'Rule Number 1')
+
+    const tickButton = container.querySelector('.tickButton')
+    act(async () => {
+      await userEvent.click(tickButton as TargetElement)
+    })
+
+    await waitFor(() => {
+      expect(getByText('Rule Number 1')).toBeInTheDocument()
+    })
+
+    expect(document.getElementsByClassName('configFormContainer')[0]).toMatchSnapshot('View Rule - 1')
+
+    // Add another Rule
+    userEvent.click(getByText('Add rule'))
+    await waitFor(() => {
+      expect(getByText('envType')).toBeInTheDocument()
+    })
+
+    const inputEl2 = document.querySelector('input[name="entity[1].name"]')
+    userEvent.type(inputEl2 as TargetElement, 'Rule Number 2')
+
+    const tickButton2 = container.querySelector('.tickButton')
+    act(async () => {
+      await userEvent.click(tickButton2 as TargetElement)
+    })
+
+    await waitFor(() => {
+      expect(getByText('Rule Number 2')).toBeInTheDocument()
+    })
+
+    expect(document.getElementsByClassName('configFormContainer')[1]).toBeInTheDocument()
+  })
+})
