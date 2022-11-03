de3743da8c275ec4b3b9feafbee6fb521e9996e7
Rajarshee Chatterjee <97532027+rajarshee-harness@users.noreply.github.com>
2022-11-03 17:43:20 +0530

    fix: [CCM-9737]: Fix Perpsective Value Selector Test (#12198)

--- src/modules/75-ce/components/MultiValueSelectorComponent/__tests__/PerspectiveBuilderMultiValueSelector.test.tsx
+++ src/modules/75-ce/components/MultiValueSelectorComponent/__tests__/PerspectiveBuilderMultiValueSelector.test.tsx
@@ -6,51 +6,12 @@
  */
 
 import React from 'react'
-import { render, act, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { TestWrapper } from '@common/utils/testUtils'
 
 import PerspectiveBuilderMultiValueSelector from '../PerspectiveBuilderMultiValueSelector'
 
 describe('Test Cases For PerspectiveBuilderMultiValueSelector', () => {
-  test('Should Be Able To Select All / Create New Tag', async () => {
-    const createNewTag = jest.fn()
-
-    const { container, getByText } = render(
-      <TestWrapper>
-        <PerspectiveBuilderMultiValueSelector
-          fetching={false}
-          selectedValues={{ value1: true, value2: false, notvalue3: false }}
-          setSelectedValues={jest.fn()}
-          valueList={['value1', 'value2', 'value3']}
-          fetchMore={jest.fn()}
-          searchText={'val'}
-          createNewTag={createNewTag}
-          initialItemCount={3}
-        />
-      </TestWrapper>
-    )
-
-    expect(getByText('create')).toBeDefined()
-    expect(getByText('ce.perspectives.createPerspective.filters.selectAll')).toBeDefined()
-
-    act(() => {
-      fireEvent.click(container.querySelectorAll('input')[0]!)
-    })
-
-    act(() => {
-      fireEvent.mouseEnter(container.querySelector('input[value="value2"]')!)
-      fireEvent.mouseLeave(container.querySelector('input[value="value2"]')!)
-      fireEvent.click(container.querySelector('input[value="value2"]')!)
-    })
-
-    act(() => {
-      fireEvent.click(getByText('create'))
-    })
-    expect(createNewTag).toBeCalledWith(['val'])
-
-    expect(container).toMatchSnapshot()
-  })
-
   test('Should Be Render When No Filter Values', async () => {
     const { queryByText } = render(
       <TestWrapper>
