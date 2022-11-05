1a72895cbf317aed45c202a62e7faf8c6df40be6
Bhavya Sinha <bhavya.sinha@harness.io>
2022-11-05 21:45:30 +0530

    feat: [CDS-46027]: primary artifact field values retained on switching from visual to yaml view (#12231)
    * feat: [CDS-46027]: primary artifact field values retained on switching from visual to yaml view
    
    * feat: [CDS-46027]: primary artifact field values retained on switching from visual to yaml view

--- src/modules/75-cd/components/PipelineSteps/K8sServiceSpec/PrimaryArtifact/PrimaryArtifactRef.tsx
+++ src/modules/75-cd/components/PipelineSteps/K8sServiceSpec/PrimaryArtifact/PrimaryArtifactRef.tsx
@@ -73,19 +73,22 @@ function PrimaryArtifactRef({
     if (
       typeof artifactSourceTemplate === 'string' &&
       getMultiTypeFromValue(artifactSourceTemplate) === MultiTypeInputType.RUNTIME &&
-      //In templateusage view type, the formik is directly set by reading the values from pipeline yaml, whereas in run pipeline form, the set value is reset on switching between yaml and visual view
-      stepViewType !== StepViewType.TemplateUsage &&
       //Autoselect primary artifact if there is only 1 artifact source
       (!isEmpty(serviceInputsFormikValue) || isSingleArtifactSource)
     ) {
+      const shouldSetDefaultArtifactSource = isSingleArtifactSource && stepViewType !== StepViewType.TemplateUsage
+
       const sourceIdentifierToSourceInputMap = get(
         artifactSourceResponse?.data?.sourceIdentifierToSourceInputMap,
-        isSingleArtifactSource ? artifactSources[0].value : `${initialValues.artifacts?.primary?.primaryArtifactRef}`
+        shouldSetDefaultArtifactSource
+          ? artifactSources[0].value
+          : `${initialValues.artifacts?.primary?.primaryArtifactRef}`
       )
       if (sourceIdentifierToSourceInputMap) {
         const idSourceMap = yamlParse(defaultTo(sourceIdentifierToSourceInputMap, ''))
         if (idSourceMap) {
-          if (isSingleArtifactSource) {
+          //In templateusage view type, the formik is directly set by reading the values from pipeline yaml, whereas in run pipeline form, the set value is reset on switching between yaml and visual view
+          if (shouldSetDefaultArtifactSource) {
             formik?.setValues(
               produce(formik?.values, (draft: any) => {
                 set(draft, `${path}.artifacts.primary.primaryArtifactRef`, artifactSources[0].value)
