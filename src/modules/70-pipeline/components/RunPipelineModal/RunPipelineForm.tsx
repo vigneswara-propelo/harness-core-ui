5efaf242f978ac7422d6449c06745d119f124d4d
Sagar-Rajput7 <sagar.rajput@harness.io>
2022-11-15 17:57:02 +0530

    feat: [PIE-6381]: Changed permission for runpipelineForm to allow edit Yaml for [EXECUTE_PIPELINE] (#12440)
    * feat: [PIE-6381]: Changed permission for runpipelineForm from EDIT_PIPELINE to EXECUTE_PIPELINE, to allow edit yaml.
    
    * feat: [PIE-6381]: CodeFix

--- src/modules/70-pipeline/components/RunPipelineModal/RunPipelineForm.tsx
+++ src/modules/70-pipeline/components/RunPipelineModal/RunPipelineForm.tsx
@@ -176,7 +176,7 @@ function RunPipelineFormBasic({
   const [yamlHandler, setYamlHandler] = useState<YamlBuilderHandlerBinding | undefined>()
   const [isInputSetApplied, setIsInputSetApplied] = useState(true)
 
-  const [canEdit] = usePermission(
+  const [canSaveInputSet, canEditYaml] = usePermission(
     {
       resourceScope: {
         accountIdentifier: accountId,
@@ -187,7 +187,7 @@ function RunPipelineFormBasic({
         resourceType: ResourceType.PIPELINE,
         resourceIdentifier: pipelineIdentifier
       },
-      permissions: [PermissionIdentifier.EDIT_PIPELINE]
+      permissions: [PermissionIdentifier.EDIT_PIPELINE, PermissionIdentifier.EXECUTE_PIPELINE]
     },
     [accountId, orgIdentifier, projectIdentifier, pipelineIdentifier]
   )
@@ -839,7 +839,7 @@ function RunPipelineFormBasic({
                           invocationMap={factory.getInvocationMap()}
                           height="55vh"
                           width="100%"
-                          isEditModeSupported={canEdit}
+                          isEditModeSupported={canEditYaml}
                           comparableYaml={inputSetYamlResponse?.data?.inputSetTemplateYaml}
                         />
                       </Layout.Vertical>
@@ -953,7 +953,7 @@ function RunPipelineFormBasic({
                         currentPipeline={{ pipeline: values }}
                         values={values}
                         template={inputSetYamlResponse?.data?.inputSetTemplateYaml}
-                        canEdit={canEdit}
+                        canEdit={canSaveInputSet}
                         accountId={accountId}
                         projectIdentifier={projectIdentifier}
                         orgIdentifier={orgIdentifier}
