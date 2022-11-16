cb34d87fe883713948094d7df316875a682b28df
Pankaj SIngh <110591046+pankajsingh-harness@users.noreply.github.com>
2022-11-16 15:57:58 +0530

    fix: [PIE-6404]: Do not call useGetSettingValue API for Github Webhook Trigger Authentication if NG_SETTINGS is disabled (#12461)
    * fix: [PIE-6404]: Do not call useGetSettingValue API for Github Webhook Trigger Authentication if NG_SETTINGS is disabled

--- src/modules/72-triggers/components/Triggers/WebhookTrigger/useIsGithubWebhookAuthenticationEnabled.tsx
+++ src/modules/72-triggers/components/Triggers/WebhookTrigger/useIsGithubWebhookAuthenticationEnabled.tsx
@@ -5,33 +5,49 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-import { useMemo } from 'react'
+import { useMemo, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { FeatureFlag } from '@common/featureFlags'
-import { useFeatureFlag } from '@common/hooks/useFeatureFlag'
+import { useToaster } from '@harness/uicore'
+import { useFeatureFlags } from '@common/hooks/useFeatureFlag'
 import { useGetSettingValue } from 'services/cd-ng'
 import type { ProjectPathProps } from '@common/interfaces/RouteInterfaces'
 import { SettingType } from '@default-settings/interfaces/SettingType.types'
+import { getErrorMessage } from '../utils'
 
 const useIsGithubWebhookAuthenticationEnabled = (): {
   isGithubWebhookAuthenticationEnabled: boolean
   isGithubWebhookAuthenticationDataLoading: boolean
 } => {
-  const isSpgNgGithubWebhookAuthenticationEnabled = useFeatureFlag(FeatureFlag.SPG_NG_GITHUB_WEBHOOK_AUTHENTICATION)
+  const { showError } = useToaster()
+  const { NG_SETTINGS } = useFeatureFlags()
   const { accountId: accountIdentifier, orgIdentifier, projectIdentifier } = useParams<ProjectPathProps>()
 
-  const { data: projectSettingData, loading: isGithubWebhookAuthenticationDataLoading } = useGetSettingValue({
+  const {
+    data: projectSettingData,
+    loading: isGithubWebhookAuthenticationDataLoading,
+    error: projectSettingDataError
+  } = useGetSettingValue({
     identifier: SettingType.WEBHOOK_GITHUB_TRIGGERS_AUTHENTICATION,
     queryParams: {
       accountIdentifier,
       orgIdentifier,
       projectIdentifier
-    }
+    },
+    lazy: !NG_SETTINGS
   })
 
-  const isGithubWebhookAuthenticationEnabled = useMemo(() => {
-    return isSpgNgGithubWebhookAuthenticationEnabled && projectSettingData?.data?.value === 'true'
-  }, [projectSettingData, isSpgNgGithubWebhookAuthenticationEnabled])
+  useEffect(() => {
+    if (projectSettingDataError) {
+      showError(getErrorMessage(projectSettingDataError))
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [projectSettingDataError])
+
+  const isGithubWebhookAuthenticationEnabled = useMemo(
+    () => projectSettingData?.data?.value === 'true',
+    [projectSettingData]
+  )
 
   return { isGithubWebhookAuthenticationEnabled, isGithubWebhookAuthenticationDataLoading }
 }
