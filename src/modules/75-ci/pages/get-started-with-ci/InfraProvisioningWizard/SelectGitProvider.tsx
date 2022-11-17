13103ec65d287f7868184352fdf0007fc04fe066
Shaurya Kalia <113332561+shaurya-harness@users.noreply.github.com>
2022-11-17 21:29:52 +0530

    fix: [CI-6164]: account scope prefix for access token (#12486)
    * fix: account scope prefix for access token
    
    * fix: typo

--- src/modules/75-ci/pages/get-started-with-ci/InfraProvisioningWizard/SelectGitProvider.tsx
+++ src/modules/75-ci/pages/get-started-with-ci/InfraProvisioningWizard/SelectGitProvider.tsx
@@ -390,10 +390,14 @@ const SelectGitProviderRef = (
           updatedConnectorPayload = set(
             updatedConnectorPayload,
             'spec.authentication.spec.spec.tokenRef',
-            `${secretId}`
+            `${ACCOUNT_SCOPE_PREFIX}${secretId}`
           )
           updatedConnectorPayload = set(updatedConnectorPayload, 'spec.apiAccess.type', 'Token')
-          updatedConnectorPayload = set(updatedConnectorPayload, 'spec.apiAccess.spec.tokenRef', `${secretId}`)
+          updatedConnectorPayload = set(
+            updatedConnectorPayload,
+            'spec.apiAccess.spec.tokenRef',
+            `${ACCOUNT_SCOPE_PREFIX}${secretId}`
+          )
           return updatedConnectorPayload
         case Connectors.BITBUCKET:
           updatedConnectorPayload = set(commonConnectorPayload, 'spec.authentication.spec.type', 'UsernamePassword')
