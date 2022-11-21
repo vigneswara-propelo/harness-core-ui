1a00bfa9c26507c9021509244b4ac146f5cda699
satyam-harness <96036463+satyam-harness@users.noreply.github.com>
2022-11-21 15:55:20 +0530

    fix: [CDS-45579]: username assertion in connectors (#12238)
    * fix: [CDS-45579]: username assertion in connectors
    
    * fix: snapshot upadted
    
    * fix: snapshots updated
    
    * fix: snapshots upadted

--- src/modules/30-secrets/components/TextReference/TextReference.tsx
+++ src/modules/30-secrets/components/TextReference/TextReference.tsx
@@ -5,7 +5,7 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import cx from 'classnames'
 import { get, isPlainObject } from 'lodash-es'
@@ -58,10 +58,6 @@ const TextReference: React.FC<FormikTextReference> = props => {
   const { accountId, projectIdentifier, orgIdentifier } = useParams<ProjectPathProps>()
   const { showError } = useToaster()
   const { formik, name, allowSelection, privateSecret } = props
-  const [username, setUsername] = useState<TextReferenceInterface>({
-    value: formik.values[`${name}textField`] || formik.values[`${name}`]?.value,
-    type: ValueType.TEXT
-  })
   const hasError = errorCheck(props.name, formik)
 
   useEffect(() => {
@@ -71,13 +67,6 @@ const TextReference: React.FC<FormikTextReference> = props => {
       formik.setFieldValue(`${name}fieldType`, ValueType.TEXT)
     }
   }, [props.type])
-
-  useEffect(() => {
-    if (formik.values[`${name}fieldType`] === ValueType.TEXT) {
-      formik.setFieldValue(name, username)
-      formik.setFieldValue(`${name}textField`, username?.value)
-    }
-  }, [formik.values[`${name}fieldType`]])
 
   useEffect(() => {
     if (formik.values[`${name}secretField`]) {
@@ -170,7 +159,6 @@ const TextReference: React.FC<FormikTextReference> = props => {
                   value: (e.target as any).value,
                   type: ValueType.TEXT
                 })
-                setUsername({ value: (e.target as any).value, type: ValueType.TEXT })
               }
             }}
             className={css.textCss}
