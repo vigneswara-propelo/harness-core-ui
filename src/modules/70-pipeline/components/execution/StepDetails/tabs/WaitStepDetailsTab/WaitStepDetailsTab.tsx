51f7a426a6e0beaa832e740a922b5d8244e93279
Ramesh Reddy <ramesh.reddy@harness.io>
2022-12-01 20:29:38 +0530

    fix: [PIE-5841]: wait step issues (#12733)

--- src/modules/70-pipeline/components/execution/StepDetails/tabs/WaitStepDetailsTab/WaitStepDetailsTab.tsx
+++ src/modules/70-pipeline/components/execution/StepDetails/tabs/WaitStepDetailsTab/WaitStepDetailsTab.tsx
@@ -12,9 +12,18 @@ import { Color } from '@harness/design-system'
 import { defaultTo } from 'lodash-es'
 import { String, useStrings } from 'framework/strings'
 import { Duration } from '@common/exports'
-import { useMarkWaitStep, ExecutionNode, useExecutionDetails, WaitStepRequestDto } from 'services/pipeline-ng'
+import {
+  useMarkWaitStep,
+  ExecutionNode,
+  useExecutionDetails,
+  WaitStepRequestDto,
+  ResponseWaitStepExecutionDetailsDto
+} from 'services/pipeline-ng'
 import { WaitActions, waitActionsIconMap, waitActionsStringMap } from '@pipeline/utils/FailureStrategyUtils'
 import type { ExecutionPathProps, PipelineType } from '@common/interfaces/RouteInterfaces'
+import { usePermission } from '@rbac/hooks/usePermission'
+import { PermissionIdentifier } from '@rbac/interfaces/PermissionIdentifier'
+import { ResourceType } from '@rbac/interfaces/ResourceType'
 import { msToTime } from './WaitStepDetailstabUtil'
 import css from './WaitStepTab.module.scss'
 
@@ -22,17 +31,88 @@ export interface WaitStepDetailsTabProps {
   step: ExecutionNode
 }
 
-// let hideSuccessButton = false
-// let hideFailButton = false
-let isDisabled = false
+interface WaitStepMarkState {
+  isMarked: boolean
+  markedAs?: WaitActions.MarkedAsSuccess | WaitActions.MarkedAsFailure
+}
+
+interface WaitStepDetailsProps {
+  step: ExecutionNode
+  details: ResponseWaitStepExecutionDetailsDto | null
+}
+
+function WaitStepDetails({ step, details }: WaitStepDetailsProps): JSX.Element {
+  const { getString } = useStrings()
+
+  const duration = details?.data?.duration
+  const daysDuration = msToTime(duration)
+  const startTime =
+    new Date(defaultTo(step.startTs, '')).toLocaleString() === 'Invalid Date'
+      ? null
+      : new Date(defaultTo(step.startTs, '')).toLocaleString()
+  const endTime =
+    new Date(defaultTo(step.endTs, '')).toLocaleString() === 'Invalid Date'
+      ? null
+      : new Date(defaultTo(step.endTs, '')).toLocaleString()
+
+  return (
+    <Container
+      color={Color.ORANGE_400}
+      background={Color.YELLOW_100}
+      padding={{ top: 'xxlarge', bottom: 'xxlarge', left: 'large', right: 'large' }}
+    >
+      <table className={css.detailsTable}>
+        <tbody>
+          <tr>
+            <th>{getString('startedAt')}</th>
+            <td>{defaultTo(startTime, '-')}</td>
+          </tr>
+          <tr>
+            <th>{getString('endedAt')}</th>
+            <td>{defaultTo(endTime, '-')}</td>
+          </tr>
+          <tr>
+            <th>{getString('duration')}</th>
+            <td>{daysDuration}</td>
+          </tr>
+          <tr>
+            <th>{getString('pipeline.execution.elapsedTime')}</th>
+            <td>
+              <div>
+                <Duration
+                  color={Color.ORANGE_400}
+                  className={css.timer}
+                  durationText=""
+                  startTime={step.startTs}
+                  endTime={step.endTs}
+                  showZeroSecondsResult
+                />
+              </div>
+            </td>
+          </tr>
+          {step?.stepDetails?.waitStepActionTaken?.actionTaken ? (
+            <tr>
+              <th>{getString('action')}</th>
+              <td>{step?.stepDetails?.waitStepActionTaken?.actionTaken}</td>
+            </tr>
+          ) : null}
+        </tbody>
+      </table>
+    </Container>
+  )
+}
 
 export function WaitStepDetailsTab(props: WaitStepDetailsTabProps): React.ReactElement {
   const { step = {} } = props
   const { getString } = useStrings()
-  const { orgIdentifier, projectIdentifier, accountId } = useParams<PipelineType<ExecutionPathProps>>()
-  const [hideFailButton, setHideFailButton] = useState(false)
-  const [hideSuccessButton, setHideSuccessButton] = useState(false)
-  const { mutate: handleInterrupt } = useMarkWaitStep({
+  const { orgIdentifier, projectIdentifier, accountId, pipelineIdentifier } =
+    useParams<PipelineType<ExecutionPathProps>>()
+  const [markState, setMarkState] = useState<WaitStepMarkState>({
+    isMarked: false,
+    markedAs: undefined
+  })
+
+  const { mutate: markWaitStep } = useMarkWaitStep({
     nodeExecutionId: step.uuid || /* istanbul ignore next */ ''
   })
 
@@ -42,84 +122,40 @@ export function WaitStepDetailsTab(props: WaitStepDetailsTabProps): React.ReactE
     orgIdentifier
   }
 
-  const { data: stepData } = useExecutionDetails({
+  const { data: executionDetails } = useExecutionDetails({
     nodeExecutionId: defaultTo(step.uuid, ''),
     queryParams: {
       ...commonParams
     }
   })
 
-  function DurationMessage() {
-    const duration = stepData?.data?.duration
-    const daysDuration = msToTime(duration)
-    const startTime =
-      new Date(defaultTo(step.startTs, '')).toLocaleString() === 'Invalid Date'
-        ? null
-        : new Date(defaultTo(step.startTs, '')).toLocaleString()
-    const endTime =
-      new Date(defaultTo(step.endTs, '')).toLocaleString() === 'Invalid Date'
-        ? null
-        : new Date(defaultTo(step.endTs, '')).toLocaleString()
+  const [canExecute] = usePermission(
+    {
+      resourceScope: {
+        accountIdentifier: accountId,
+        orgIdentifier,
+        projectIdentifier
+      },
+      resource: {
+        resourceType: ResourceType.PIPELINE,
+        resourceIdentifier: pipelineIdentifier as string
+      },
+      permissions: [PermissionIdentifier.EXECUTE_PIPELINE]
+    },
+    [orgIdentifier, projectIdentifier, accountId, pipelineIdentifier]
+  )
 
-    return (
-      <Container
-        color={Color.ORANGE_400}
-        background={Color.YELLOW_100}
-        padding={{ top: 'xxlarge', bottom: 'xxlarge', left: 'large', right: 'large' }}
-      >
-        <table className={css.detailsTable}>
-          <tbody>
-            <tr>
-              <th>{getString('startedAt')}</th>
-              <td>{defaultTo(startTime, '-')}</td>
-            </tr>
-            <tr>
-              <th>{getString('endedAt')}</th>
-              <td>{defaultTo(endTime, '-')}</td>
-            </tr>
-            <tr>
-              <th>{getString('duration')}</th>
-              <td>{daysDuration}</td>
-            </tr>
-            <tr>
-              <th>{getString('pipeline.execution.elapsedTime')}</th>
-              <td>
-                <div>
-                  <Duration
-                    color={Color.ORANGE_400}
-                    className={css.timer}
-                    durationText=""
-                    startTime={step.startTs}
-                    endTime={step.endTs}
-                    showZeroSecondsResult
-                  />
-                </div>
-              </td>
-            </tr>
-            {step?.stepDetails?.waitStepActionTaken?.actionTaken ? (
-              <tr>
-                <th>{getString('action')}</th>
-                <td>{step?.stepDetails?.waitStepActionTaken?.actionTaken}</td>
-              </tr>
-            ) : null}
-          </tbody>
-        </table>
-      </Container>
-    )
-  }
+  const onClick = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void => {
+    const action: WaitStepRequestDto['action'] =
+      value === WaitActions.MarkAsSuccess ? 'MARK_AS_SUCCESS' : 'MARK_AS_FAIL'
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
-    isDisabled = true
-    const actionPassed = (
-      e.target.value === 'MarkAsSuccess' ? 'MARK_AS_SUCCESS' : 'MARK_AS_FAIL'
-    ) as WaitStepRequestDto['action']
-    if (e.target.value === 'MarkAsSuccess') {
-      setHideFailButton(true)
-    } else {
-      setHideSuccessButton(true)
-    }
-    const waitStepRequestDto = { action: actionPassed }
-    handleInterrupt(waitStepRequestDto, {
+    setMarkState({
+      isMarked: true,
+      markedAs: value === WaitActions.MarkAsSuccess ? WaitActions.MarkedAsSuccess : WaitActions.MarkedAsFailure
+    })
+
+    const waitStepRequestDto = { action }
+    markWaitStep(waitStepRequestDto, {
       queryParams: {
         accountIdentifier: accountId,
         orgIdentifier,
@@ -128,45 +164,46 @@ export function WaitStepDetailsTab(props: WaitStepDetailsTabProps): React.ReactE
       headers: { 'content-type': 'application/json' }
     })
   }
-  const status = step?.status === 'WaitStepRunning' ? true : false
+
+  const isRunning = step?.status === 'WaitStepRunning'
+  const { isMarked, markedAs } = markState
+
   return (
     <React.Fragment>
-      <DurationMessage />
-      {step?.status === 'WaitStepRunning' ? (
+      <WaitStepDetails details={executionDetails} step={step} />
+      {isRunning && (
         <div className={css.manualInterventionTab}>
           <String tagName="div" className={css.title} stringID="common.PermissibleActions" />
           <div className={css.actionRow}>
             <Thumbnail
-              disabled={isDisabled === true && status === false ? true : false}
-              key={0}
+              disabled={!canExecute || isMarked}
               label={
-                hideFailButton
+                isMarked && markedAs === WaitActions.MarkedAsSuccess
                   ? getString(waitActionsStringMap[WaitActions.MarkedAsSuccess])
                   : getString(waitActionsStringMap[WaitActions.MarkAsSuccess])
               }
               icon={waitActionsIconMap[WaitActions.MarkAsSuccess]}
               value={WaitActions.MarkAsSuccess}
               name={WaitActions.MarkAsSuccess}
-              onClick={handleChange}
+              onClick={onClick}
               className={css.thumbnail}
             />
             <Thumbnail
-              disabled={isDisabled === true && status === false ? true : false}
-              key={0}
+              disabled={!canExecute || isMarked}
               label={
-                hideSuccessButton
+                isMarked && markedAs === WaitActions.MarkedAsFailure
                   ? getString(waitActionsStringMap[WaitActions.MarkedAsFailure])
                   : getString(waitActionsStringMap[WaitActions.MarkAsFailure])
               }
               icon={waitActionsIconMap[WaitActions.MarkAsFailure]}
               value={WaitActions.MarkAsFailure}
               name={WaitActions.MarkAsFailure}
-              onClick={handleChange}
+              onClick={onClick}
               className={css.thumbnail}
             />
           </div>
         </div>
-      ) : null}
+      )}
     </React.Fragment>
   )
 }
