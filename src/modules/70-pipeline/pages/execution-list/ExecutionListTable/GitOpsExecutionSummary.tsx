1c807a81e9e4777b63490e6b3ed6c3e83d2dfc8e
Ishant Mehta <43949150+ishantmehtaharness@users.noreply.github.com>
2022-12-07 01:10:45 +0530

    feat: [CDS-47800]: Gitops fetch linked apps - Applications should be … (#12864)
    * feat: [CDS-47800]: Gitops fetch linked apps - Applications should be clickable
    
    * feat: [CDS-47800]: Gitops fetch linked apps - Applications should be clickable

--- src/modules/70-pipeline/pages/execution-list/ExecutionListTable/GitOpsExecutionSummary.tsx
+++ src/modules/70-pipeline/pages/execution-list/ExecutionListTable/GitOpsExecutionSummary.tsx
@@ -6,30 +6,56 @@
  */
 
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
 import { Color, FontVariation } from '@harness/design-system'
 import { Classes, PopoverInteractionKind } from '@blueprintjs/core'
 import { Icon, Layout, Popover, Text } from '@harness/uicore'
 import { useStrings } from 'framework/strings'
 import type { Application } from 'services/cd-ng'
+import routes from '@common/RouteDefinitions'
+import type { ModulePathParams, ProjectPathProps } from '@common/interfaces/RouteInterfaces'
 
 function GitOpsExecutionSummary({ stageInfo, limit = 1 }: { stageInfo: Record<string, any>; limit?: number }) {
   const { getString } = useStrings()
+  const { orgIdentifier, projectIdentifier, accountId, module } = useParams<ProjectPathProps & ModulePathParams>()
   const gitOpsApplications = stageInfo.gitOpsAppSummary?.applications || []
+
+  const linkNode = (app: Application, index: number, color: Color) => {
+    return (
+      <Link
+        key={app.identifier || app.name}
+        onClick={e => e.stopPropagation()}
+        to={routes.toGitOpsApplication({
+          orgIdentifier,
+          projectIdentifier,
+          accountId,
+          module,
+          applicationId: (app.identifier || app.name) as string,
+          agentId: app.agentIdentifier
+        })}
+      >
+        <Text font={{ variation: FontVariation.SMALL_SEMI }} color={color} key={app.identifier || index}>
+          {app.name}
+        </Text>
+      </Link>
+    )
+  }
 
   return gitOpsApplications.length ? (
     <Layout.Horizontal>
-      <Layout.Horizontal spacing="xsmall" style={{ alignItems: 'center' }}>
+      <Layout.Horizontal
+        spacing="xsmall"
+        style={{ alignItems: 'center' }}
+        margin={{ left: 'small' }}
+        padding={{ left: 'xsmall' }}
+      >
         <Icon name="gitops-application" size={14} />
         <Text font={{ variation: FontVariation.SMALL }} color={Color.GREY_600}>
           {getString('applications')}:
         </Text>
         <Text font={{ variation: FontVariation.SMALL_SEMI }} color={Color.GREY_800}>
           {gitOpsApplications.slice(0, limit).map((app: Application, index: number) => {
-            return (
-              <Text font={{ variation: FontVariation.SMALL_SEMI }} color={Color.GREY_800} key={app.identifier || index}>
-                {app.name}
-              </Text>
-            )
+            return linkNode(app, index, Color.GREY_800)
           })}
         </Text>
         {gitOpsApplications.length > limit ? (
@@ -39,17 +65,9 @@ function GitOpsExecutionSummary({ stageInfo, limit = 1 }: { stageInfo: Record<st
               className={Classes.DARK}
               content={
                 <Layout.Vertical spacing="small" padding="medium" style={{ maxWidth: 500 }}>
-                  {gitOpsApplications.slice(limit).map((app: Application, index: number) => {
-                    return (
-                      <Text
-                        font={{ variation: FontVariation.SMALL_SEMI }}
-                        color={Color.WHITE}
-                        key={app.identifier || index}
-                      >
-                        {app.name}
-                      </Text>
-                    )
-                  })}
+                  {gitOpsApplications
+                    .slice(limit)
+                    .map((app: Application, index: number) => linkNode(app, index, Color.WHITE))}
                 </Layout.Vertical>
               }
             >
--- src/modules/70-pipeline/pages/execution-list/ExecutionListTable/__tests__/GitOpsExecutionSummary.test.tsx
+++ src/modules/70-pipeline/pages/execution-list/ExecutionListTable/__tests__/GitOpsExecutionSummary.test.tsx
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
 import React from 'react'
 import { render } from '@testing-library/react'
 import { TestWrapper } from '@common/utils/testUtils'
@@ -20,7 +27,10 @@ describe('GitOpsExecutionSummary', () => {
 
   test('render with one application', () => {
     const { container } = render(
-      <TestWrapper>
+      <TestWrapper
+        path="/account/:accountId/:module/orgs/:orgIdentifier/projects/:projectIdentifier/gitops/applications/:applicationId"
+        pathParams={{ projectIdentifier, orgIdentifier, accountId, module: 'cd', applicationId: 'applicationId' }}
+      >
         <GitOpsExecutionSummary
           stageInfo={{
             gitOpsAppSummary: { applications: [{ agentIdentifier: 'AGENT_1', identifier: 'ID_1', name: 'NAME_1' }] }
@@ -35,7 +45,10 @@ describe('GitOpsExecutionSummary', () => {
 
   test('render with multiple applications', () => {
     const { container } = render(
-      <TestWrapper>
+      <TestWrapper
+        path="/account/:accountId/:module/orgs/:orgIdentifier/projects/:projectIdentifier/gitops/applications/:applicationId"
+        pathParams={{ projectIdentifier, orgIdentifier, accountId, module: 'cd', applicationId: 'applicationId' }}
+      >
         <GitOpsExecutionSummary
           stageInfo={{
             gitOpsAppSummary: {
