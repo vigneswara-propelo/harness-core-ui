80ccc2039c99bf2f28adf2a2e6690d7faadc1a38
Sergey Bobrov <sergey.bobrov@harness.io>
2022-12-27 09:24:31 -0600

    fix: STO template with input variables (#13187)

--- src/modules/85-sto/components/PipelineSteps/ZeroNorthStep/ZeroNorthStep.tsx
+++ src/modules/85-sto/components/PipelineSteps/ZeroNorthStep/ZeroNorthStep.tsx
@@ -130,30 +130,27 @@ export class ZeroNorthStep extends PipelineStep<ZeroNorthStepData> {
       allowableTypes
     } = props
 
-    switch (stepViewType) {
-      case StepViewType.InputSet:
-      case StepViewType.DeploymentForm:
-        return (
-          <ZeroNorthStepInputSet
-            initialValues={initialValues}
-            template={inputSetData?.template}
-            path={inputSetData?.path || ''}
-            readonly={!!inputSetData?.readonly}
-            stepViewType={stepViewType}
-            onUpdate={onUpdate}
-            onChange={onChange}
-            allowableTypes={allowableTypes}
-          />
-        )
-
-      case StepViewType.InputVariable:
-        return (
-          <ZeroNorthStepVariables
-            {...(customStepProps as ZeroNorthStepVariablesProps)}
-            initialValues={initialValues}
-            onUpdate={onUpdate}
-          />
-        )
+    if (this.isTemplatizedView(stepViewType)) {
+      return (
+        <ZeroNorthStepInputSet
+          initialValues={initialValues}
+          template={inputSetData?.template}
+          path={inputSetData?.path || ''}
+          readonly={!!inputSetData?.readonly}
+          stepViewType={stepViewType}
+          onUpdate={onUpdate}
+          onChange={onChange}
+          allowableTypes={allowableTypes}
+        />
+      )
+    } else if (stepViewType === StepViewType.InputVariable) {
+      return (
+        <ZeroNorthStepVariables
+          {...(customStepProps as ZeroNorthStepVariablesProps)}
+          initialValues={initialValues}
+          onUpdate={onUpdate}
+        />
+      )
     }
 
     return (
