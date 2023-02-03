2fa849aa9681d6e89019b3d685012da33822ffd0
AshwinBhatkal-Harness <96711906+AshwinBhatkal-Harness@users.noreply.github.com>
2023-02-03 15:12:23 +0530

    fix: [CDS-52864]: page crash on entering fixed environment inputs (#13873)

--- src/modules/70-pipeline/components/PipelineInputSetForm/EnvironmentsInputSetForm/SingleEnvironmentInputSetForm.tsx
+++ src/modules/70-pipeline/components/PipelineInputSetForm/EnvironmentsInputSetForm/SingleEnvironmentInputSetForm.tsx
@@ -7,7 +7,7 @@
 
 import React from 'react'
 import { useFormikContext } from 'formik'
-import { get, set, unset } from 'lodash-es'
+import { get, isEmpty, set, unset } from 'lodash-es'
 import cx from 'classnames'
 
 import { RUNTIME_INPUT_VALUE } from '@harness/uicore'
@@ -45,105 +45,108 @@ export default function SingleEnvironmentInputSetForm({
 
   return (
     <>
-      {isSvcEnvEntityEnabled && deploymentStageTemplate?.environment && deploymentStage?.environment && (
-        <div id={`Stage.${stageIdentifier}.Environment`} className={cx(css.accordionSummary)}>
-          <StepWidget
-            factory={factory}
-            initialValues={deploymentStageInputSet}
-            allowableTypes={allowableTypes}
-            allValues={deploymentStage}
-            template={deploymentStageTemplate}
-            type={StepType.DeployInfrastructure}
-            stepViewType={viewType}
-            path={`${path}.environment`}
-            readonly={readonly}
-            customStepProps={{
-              getString,
-              // Show clusters instead of infra on env selection
-              gitOpsEnabled: deploymentStage.gitOpsEnabled,
-              // load service overrides for environment
-              serviceRef: deploymentStage.service?.serviceRef,
-              // load infrastructures/clusters in environment
-              environmentRef: deploymentStage.environment?.environmentRef,
-              // load infrastructure runtime inputs
-              infrastructureRef: deploymentStage.environment?.infrastructureDefinitions?.[0].identifier,
-              // load cluster runtime inputs
-              clusterRef: deploymentStage.environment?.gitOpsClusters?.[0].identifier,
-              // required for artifact manifest inputs
-              stageIdentifier,
-              // required for filtering infrastructures
-              deploymentType: deploymentStage?.deploymentType,
-              customDeploymentData: deploymentStage?.customDeploymentRef
-            }}
-            onUpdate={values => {
-              if (deploymentStageInputSet?.environment) {
-                formik?.setFieldValue(`${path}.environment`, values.environment)
-              }
-            }}
-          />
+      {isSvcEnvEntityEnabled &&
+        deploymentStageTemplate?.environment &&
+        deploymentStage?.environment &&
+        !isEmpty(deploymentStageInputSet.environment) && (
+          <div id={`Stage.${stageIdentifier}.Environment`} className={cx(css.accordionSummary)}>
+            <StepWidget
+              factory={factory}
+              initialValues={deploymentStageInputSet}
+              allowableTypes={allowableTypes}
+              allValues={deploymentStage}
+              template={deploymentStageTemplate}
+              type={StepType.DeployInfrastructure}
+              stepViewType={viewType}
+              path={`${path}.environment`}
+              readonly={readonly}
+              customStepProps={{
+                getString,
+                // Show clusters instead of infra on env selection
+                gitOpsEnabled: deploymentStage.gitOpsEnabled,
+                // load service overrides for environment
+                serviceRef: deploymentStage.service?.serviceRef,
+                // load infrastructures/clusters in environment
+                environmentRef: deploymentStage.environment?.environmentRef,
+                // load infrastructure runtime inputs
+                infrastructureRef: deploymentStage.environment?.infrastructureDefinitions?.[0].identifier,
+                // load cluster runtime inputs
+                clusterRef: deploymentStage.environment?.gitOpsClusters?.[0].identifier,
+                // required for artifact manifest inputs
+                stageIdentifier,
+                // required for filtering infrastructures
+                deploymentType: deploymentStage?.deploymentType,
+                customDeploymentData: deploymentStage?.customDeploymentRef
+              }}
+              onUpdate={values => {
+                if (deploymentStageInputSet?.environment) {
+                  formik?.setFieldValue(`${path}.environment`, values.environment)
+                }
+              }}
+            />
 
-          {(deploymentStageTemplate as DeployStageConfig).environment?.infrastructureDefinitions &&
-            ((deploymentStageTemplate as DeployStageConfig).environment
-              ?.infrastructureDefinitions as unknown as string) !== RUNTIME_INPUT_VALUE && (
-              <>
-                {deploymentStage.environment?.environmentRef &&
-                  ((deploymentStage as DeployStageConfig)?.environment
-                    ?.infrastructureDefinitions as unknown as string) !== RUNTIME_INPUT_VALUE && (
-                    <div className={css.inputheader}>{getString('infrastructureText')}</div>
-                  )}
-                {deploymentStageTemplate.environment?.infrastructureDefinitions
-                  ?.map((infrastructureDefinition, index) => {
-                    return (
-                      <>
-                        <StepWidget<Infrastructure>
-                          key={infrastructureDefinition.identifier}
-                          factory={factory}
-                          template={infrastructureDefinition.inputs?.spec}
-                          initialValues={{
-                            ...deploymentStageInputSet?.environment?.infrastructureDefinitions?.[index]?.inputs?.spec,
-                            environmentRef: deploymentStage?.environment?.environmentRef,
-                            infrastructureRef: infrastructureDefinition.identifier,
-                            deploymentType: deploymentStage?.deploymentType
-                          }}
-                          allowableTypes={allowableTypes}
-                          allValues={{
-                            ...deploymentStage?.environment?.infrastructureDefinitions?.[index]?.inputs?.spec,
-                            environmentRef: deploymentStage?.environment?.environmentRef,
-                            infrastructureRef: infrastructureDefinition.identifier
-                          }}
-                          type={
-                            (infraDefinitionTypeMapping[infrastructureDefinition?.inputs?.type as StepType] ||
-                              infrastructureDefinition?.inputs?.type) as StepType
-                          }
-                          path={`${path}.environment.infrastructureDefinitions.${index}.inputs.spec`}
-                          readonly={readonly}
-                          stepViewType={viewType}
-                          customStepProps={{
-                            ...getCustomStepProps((deploymentStage?.deploymentType as StepType) || '', getString),
-                            serviceRef: deploymentStage?.service?.serviceRef,
-                            environmentRef: deploymentStage?.environment?.environmentRef,
-                            infrastructureRef: deploymentStage?.environment?.infrastructureDefinitions?.[0].identifier
-                          }}
-                          onUpdate={data => {
-                            /* istanbul ignore next */
-                            if (
-                              deploymentStageInputSet?.environment?.infrastructureDefinitions?.[index]?.inputs?.spec
-                            ) {
-                              unset(data, 'environmentRef')
-                              unset(data, 'infrastructureRef')
-                              deploymentStageInputSet.environment.infrastructureDefinitions[index].inputs.spec = data
-                              formik?.setValues(set(formik?.values, path, deploymentStageInputSet))
+            {(deploymentStageTemplate as DeployStageConfig).environment?.infrastructureDefinitions &&
+              ((deploymentStageTemplate as DeployStageConfig).environment
+                ?.infrastructureDefinitions as unknown as string) !== RUNTIME_INPUT_VALUE && (
+                <>
+                  {deploymentStage.environment?.environmentRef &&
+                    ((deploymentStage as DeployStageConfig)?.environment
+                      ?.infrastructureDefinitions as unknown as string) !== RUNTIME_INPUT_VALUE && (
+                      <div className={css.inputheader}>{getString('infrastructureText')}</div>
+                    )}
+                  {deploymentStageTemplate.environment?.infrastructureDefinitions
+                    ?.map((infrastructureDefinition, index) => {
+                      return (
+                        <>
+                          <StepWidget<Infrastructure>
+                            key={infrastructureDefinition.identifier}
+                            factory={factory}
+                            template={infrastructureDefinition.inputs?.spec}
+                            initialValues={{
+                              ...deploymentStageInputSet?.environment?.infrastructureDefinitions?.[index]?.inputs?.spec,
+                              environmentRef: deploymentStage?.environment?.environmentRef,
+                              infrastructureRef: infrastructureDefinition.identifier,
+                              deploymentType: deploymentStage?.deploymentType
+                            }}
+                            allowableTypes={allowableTypes}
+                            allValues={{
+                              ...deploymentStage?.environment?.infrastructureDefinitions?.[index]?.inputs?.spec,
+                              environmentRef: deploymentStage?.environment?.environmentRef,
+                              infrastructureRef: infrastructureDefinition.identifier
+                            }}
+                            type={
+                              (infraDefinitionTypeMapping[infrastructureDefinition?.inputs?.type as StepType] ||
+                                infrastructureDefinition?.inputs?.type) as StepType
                             }
-                          }}
-                        />
-                      </>
-                    )
-                  })
-                  .filter(data => data)}
-              </>
-            )}
-        </div>
-      )}
+                            path={`${path}.environment.infrastructureDefinitions.${index}.inputs.spec`}
+                            readonly={readonly}
+                            stepViewType={viewType}
+                            customStepProps={{
+                              ...getCustomStepProps((deploymentStage?.deploymentType as StepType) || '', getString),
+                              serviceRef: deploymentStage?.service?.serviceRef,
+                              environmentRef: deploymentStage?.environment?.environmentRef,
+                              infrastructureRef: deploymentStage?.environment?.infrastructureDefinitions?.[0].identifier
+                            }}
+                            onUpdate={data => {
+                              /* istanbul ignore next */
+                              if (
+                                deploymentStageInputSet?.environment?.infrastructureDefinitions?.[index]?.inputs?.spec
+                              ) {
+                                unset(data, 'environmentRef')
+                                unset(data, 'infrastructureRef')
+                                deploymentStageInputSet.environment.infrastructureDefinitions[index].inputs.spec = data
+                                formik?.setValues(set(formik?.values, path, deploymentStageInputSet))
+                              }
+                            }}
+                          />
+                        </>
+                      )
+                    })
+                    .filter(data => data)}
+                </>
+              )}
+          </div>
+        )}
     </>
   )
 }
