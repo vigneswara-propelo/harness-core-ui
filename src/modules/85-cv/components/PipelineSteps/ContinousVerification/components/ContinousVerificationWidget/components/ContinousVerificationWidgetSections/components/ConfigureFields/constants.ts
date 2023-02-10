ddaf132456c47bae726114b06b833522d63bcaf8
praneshg239 <95267551+praneshg239@users.noreply.github.com>
2023-02-10 10:30:17 +0530

    fix: [SRM-13458]: Updated artifact tag default value in verify step (#14002)

--- src/modules/85-cv/components/PipelineSteps/ContinousVerification/components/ContinousVerificationWidget/components/ContinousVerificationWidgetSections/components/ConfigureFields/constants.ts
+++ src/modules/85-cv/components/PipelineSteps/ContinousVerification/components/ContinousVerificationWidget/components/ContinousVerificationWidgetSections/components/ConfigureFields/constants.ts
@@ -5,7 +5,7 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-export const defaultDeploymentTag = '<+serviceConfig.artifacts.primary.tag>'
+export const defaultDeploymentTag = '<+artifacts.primary.tag>'
 
 export enum VerificationTypes {
   LoadTest = 'LoadTest',
