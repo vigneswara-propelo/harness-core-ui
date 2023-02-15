82fe2eb83ba88a74a2af8c3a5125e62cf931a616
Samarth Sharma <96409598+samarth-sharma31@users.noreply.github.com>
2023-02-15 15:29:15 +0530

    fix: [CDS-53467]: Support added for showing MI tab for service now (#14082)

--- src/modules/70-pipeline/components/execution/StepDetails/views/ServiceNowCreateUpdateView/ServiceNowCreateUpdateView.tsx
+++ src/modules/70-pipeline/components/execution/StepDetails/views/ServiceNowCreateUpdateView/ServiceNowCreateUpdateView.tsx
@@ -14,8 +14,9 @@ import type { ExecutionNode } from 'services/pipeline-ng'
 import type { StepDetailProps } from '@pipeline/factories/ExecutionFactory/types'
 import { InputOutputTab } from '@pipeline/components/execution/StepDetails/tabs/InputOutputTab/InputOutputTab'
 import { ExecutionInputs } from '@pipeline/components/execution/StepDetails/tabs/ExecutionInputs/ExecutionInputs'
-import { isExecutionWaitingForInput } from '@pipeline/utils/statusHelpers'
-
+import { isExecutionWaitingForInput, isExecutionWaitingForIntervention } from '@pipeline/utils/statusHelpers'
+import { ManualInterventionTab } from '@pipeline/components/execution/StepDetails/tabs/ManualInterventionTab/ManualInterventionTab'
+import { StageType } from '@pipeline/utils/stageHelpers'
 import { StepDetailsTab } from '../../tabs/StepDetailsTab/StepDetailsTab'
 import { PolicyEvaluationContent } from '../../common/ExecutionContent/PolicyEvaluationContent/PolicyEvaluationContent'
 import tabCss from '../DefaultView/DefaultView.module.scss'
@@ -26,6 +27,7 @@ enum ApprovalStepTab {
   STEP_DETAILS = 'STEP_DETAILS',
   INPUT = 'INPUT',
   OUTPUT = 'OUTPUT',
+  MANUAL_INTERVENTION = 'MANUAL_INTERVENTION',
   STEP_EXECUTION_INPUTS = 'STEP_EXECUTION_INPUTS',
   POLICY_ENFORCEMENT = 'POLICY_ENFORCEMENT'
 }
@@ -35,7 +37,7 @@ export interface ServiceNowCreateUpdateViewProps extends StepDetailProps {
 }
 
 export function ServiceNowCreateUpdateView(props: ServiceNowCreateUpdateViewProps): React.ReactElement | null {
-  const { step, executionMetadata } = props
+  const { step, stageType = StageType.DEPLOY, executionMetadata } = props
   const ticket = get(step, 'outcomes.ticket', {}) as { ticketNumber: string; ticketUrl: string }
   const { getString } = useStrings()
   const manuallySelected = React.useRef(false)
@@ -43,6 +45,7 @@ export function ServiceNowCreateUpdateView(props: ServiceNowCreateUpdateViewProp
   const isWaitingOnExecInputs = isExecutionWaitingForInput(step.status)
   const shouldShowExecutionInputs = !!step.executionInputConfigured
   const shouldShowPolicyEnforcement = !!step?.outcomes?.policyOutput?.policySetDetails
+  const isManualInterruption = isExecutionWaitingForIntervention(step.status)
 
   const labels = []
   if (!isEmpty(ticket?.ticketUrl)) {
@@ -61,10 +64,12 @@ export function ServiceNowCreateUpdateView(props: ServiceNowCreateUpdateViewProp
       let tab = ApprovalStepTab.STEP_DETAILS
       if (isWaitingOnExecInputs) {
         tab = ApprovalStepTab.STEP_EXECUTION_INPUTS
+      } else if (isManualInterruption) {
+        tab = ApprovalStepTab.MANUAL_INTERVENTION
       }
       setActiveTab(tab)
     }
-  }, [step.identifier, isWaitingOnExecInputs])
+  }, [step.identifier, isWaitingOnExecInputs, isManualInterruption])
 
   return (
     <Tabs
@@ -105,6 +110,14 @@ export function ServiceNowCreateUpdateView(props: ServiceNowCreateUpdateViewProp
           />
         }
       />
+      {isManualInterruption && (
+        <Tabs.Tab
+          id={ApprovalStepTab.MANUAL_INTERVENTION}
+          key={ApprovalStepTab.MANUAL_INTERVENTION}
+          title={getString('pipeline.failureStrategies.strategiesLabel.ManualIntervention')}
+          panel={<ManualInterventionTab step={step} stageType={stageType} executionMetadata={executionMetadata} />}
+        />
+      )}
       {shouldShowPolicyEnforcement ? (
         <Tabs.Tab
           id={ApprovalStepTab.POLICY_ENFORCEMENT}
