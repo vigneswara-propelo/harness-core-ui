42155b7ba387d0d5e476449ec112b771bdbe759d
Sonali Goyal <sonali.goyal@harness.io>
2023-02-22 14:36:12 +0530

    feat: [CDS-53777]: Add aws and gcp artifact connectors for container registry (#14194)

--- src/modules/75-cd/components/PipelineSteps/ContainerStep/ContainerStepBase.tsx
+++ src/modules/75-cd/components/PipelineSteps/ContainerStep/ContainerStepBase.tsx
@@ -84,7 +84,7 @@ export const ContainerStepBase = (
               <FormMultiTypeConnectorField
                 name="spec.connectorRef"
                 label={getString('pipelineSteps.connectorLabel')}
-                type={Connectors.DOCKER}
+                type={[Connectors.GCP, Connectors.AWS, Connectors.DOCKER]}
                 placeholder={getString('select')}
                 disabled={readonly}
                 accountIdentifier={accountId}
