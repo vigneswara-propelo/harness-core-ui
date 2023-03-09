400f1c069a358d65931a047730fb2564c547a7ad
deepesh-ui <81554936+deepesh-ui@users.noreply.github.com>
2023-03-09 19:55:04 +0530

    feat: [SRM-14183]: unskipped test (#14528)

--- src/modules/85-cv/pages/slos/CVSLODetailsPage/DetailsPanel/__tests__/DetailsPanel.test.tsx
+++ src/modules/85-cv/pages/slos/CVSLODetailsPage/DetailsPanel/__tests__/DetailsPanel.test.tsx
@@ -50,8 +50,8 @@ jest.mock('services/cv', () => ({
     }
   })
 }))
-// eslint-disable-next-line jest/no-disabled-tests
-describe.skip('Test DetailsPanel', () => {
+
+describe('Test DetailsPanel', () => {
   test('should render DetailsPanel', () => {
     const { container, getByTestId } = render(
       <TestWrapper>
