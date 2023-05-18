85a93dda7cdc89f6e6dffde9fcb0a45e6e7d9b28
dhruv upadhyay <dhruv.upadhyay@harness.io>
2023-05-18 20:33:09 +0530

    feat: [PLG-2378]: updating labels and prices for ci (#15860)
    * feat: [PLG-2378]: updating labels and prices for ci
    
    * feat: [PLG-2378]: updating labels and prices for ci
    
    * feat: [PLG-2378]: updating labels and prices for ci qa hotfix

--- src/modules/33-auth-settings/components/Subscription/PricePreview/PricePreviewLine.tsx
+++ src/modules/33-auth-settings/components/Subscription/PricePreview/PricePreviewLine.tsx
@@ -39,11 +39,16 @@ export const PricePreviewLine: React.FC<LineProps> = ({
   const { getString } = useStrings()
   const isDevPreview = description?.includes('developers')
   const breakdownDescr = isDevPreview
-    ? `${quantity}${isDevPreview ? '' : unit} x ${getAmountInCurrency(CurrencyType.USD, unitPrice)} ${getString(
-        unitDescription as keyof StringsMap,
-        { quantity: minValue }
-      )}`
-    : `${mauValue}${isDevPreview ? '' : unit} ${getString(description as keyof StringsMap)}`
+    ? `${quantity}${isDevPreview ? '' : unit}  ${getString(
+        quantity > 1 ? 'common.subscriptions.usage.developers' : 'common.subscriptions.usage.developer'
+      )}/year`
+    : `${mauValue}${isDevPreview ? '' : unit} ${getString(description as keyof StringsMap)}/year`
+
+  const breakdownDescrMonthly = isDevPreview
+    ? `${quantity}${isDevPreview ? '' : unit}  ${getString(
+        quantity > 1 ? 'common.subscriptions.usage.developers' : 'common.subscriptions.usage.developer'
+      )}/month`
+    : `${mauValue}${isDevPreview ? '' : unit} ${getString(description as keyof StringsMap)}/month`
   const amount = isDevPreview ? quantity * unitPrice : unitPrice
   let unitDescr
   if (unitDescription && underComment) {
@@ -58,27 +63,42 @@ export const PricePreviewLine: React.FC<LineProps> = ({
   } else if (unitDescription) {
     unitDescr = <Text>{breakdownDescr}</Text>
   }
+  let unitDescrMonthly
+  if (unitDescription && underComment) {
+    unitDescrMonthly = (
+      <Layout.Vertical>
+        <Text>{breakdownDescrMonthly}</Text>
+        <Text font={{ size: 'xsmall' }}>
+          {minValue ? getString(underComment as keyof StringsMap, { minValue: `${minValue}${unit}` }) : null}
+        </Text>
+      </Layout.Vertical>
+    )
+  } else if (unitDescription) {
+    unitDescrMonthly = <Text>{breakdownDescrMonthly}</Text>
+  }
 
   return (
     <Layout.Vertical className={css.line} padding={{ top: 'small', bottom: 'small' }}>
       <Layout.Horizontal flex={{ justifyContent: 'space-between' }} padding={{ bottom: 'small' }}>
-        <Text font={{ weight: 'semi-bold' }}>{getString(description as keyof StringsMap)}</Text>
-        {paymentFrequency === TimeType.YEARLY && (
+        {unitDescrMonthly}
+        {
           <Text font={{ weight: 'semi-bold' }}>
             {getAmountInCurrency(CurrencyType.USD, paymentFrequency === TimeType.YEARLY ? amount / 12 : amount)}
             {getString('common.perMonth')}
           </Text>
-        )}
+        }
       </Layout.Horizontal>
-      <Layout.Horizontal flex={{ justifyContent: 'space-between', alignItems: 'start' }}>
-        {unitDescr}
-        {amount && (
-          <Text>
-            {getAmountInCurrency(CurrencyType.USD, amount)}
-            {paymentFrequency === TimeType.YEARLY && getString('common.perYear')}
-          </Text>
-        )}
-      </Layout.Horizontal>
+      {paymentFrequency === TimeType.YEARLY ? (
+        <Layout.Horizontal flex={{ justifyContent: 'space-between', alignItems: 'start' }}>
+          {unitDescr}
+          {amount && (
+            <Text>
+              {getAmountInCurrency(CurrencyType.USD, amount)}
+              {paymentFrequency === TimeType.YEARLY && getString('common.perYear')}
+            </Text>
+          )}
+        </Layout.Horizontal>
+      ) : null}
     </Layout.Vertical>
   )
 }
