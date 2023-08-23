5da7f0f98768831fa5cb59955e6658be5c757b31
Fardeen Kamal <fardeen.kamal@harness.io>
2023-08-24 01:43:34 +0530

    fix: [CDS-76793]: fixed step log not appearing in console when unitProgresses field is empty (#17526)

--- src/modules/70-pipeline/components/LogsContent/LogsState/__tests__/createSections.test.ts
+++ src/modules/70-pipeline/components/LogsContent/LogsState/__tests__/createSections.test.ts
@@ -9,16 +9,52 @@ import { createSections } from '../createSections'
 import { ActionType } from '../types'
 import { getDefaultReducerState } from '../utils'
 
+const selectedStep = 'SELECTED_STEP'
+const selectedStage = 'SELECTED_STAGE'
+const init = getDefaultReducerState()
+const statuses = ['SUCCESS', 'SUCCESS', 'FAILED']
+
 describe('createSections tests', () => {
   test('handles no node', () => {
-    const selectedStep = 'SELECTED_STEP'
-    const selectedStage = 'SELECTED_STAGE'
-    const init = getDefaultReducerState()
     const data = createSections(init, {
       type: ActionType.CreateSections,
       payload: { selectedStep, selectedStage, getSectionName: jest.fn() }
     })
 
     expect(data).toEqual(expect.objectContaining({ selectedStep, selectedStage }))
+  })
+
+  test('handles node progressData', () => {
+    const data = createSections(init, {
+      type: ActionType.CreateSections,
+      payload: {
+        selectedStep,
+        selectedStage,
+        getSectionName: jest.fn(),
+        node: {
+          executableResponses: [
+            {
+              taskChain: {
+                logKeys: statuses.map((_, i) => `logKey${i + 1}`),
+                units: statuses.map((_, i) => `Unit ${i + 1}`)
+              } as any
+            }
+          ],
+          progressData: {
+            unitProgresses: statuses.map((status, i) => ({ unitName: `Unit ${i + 1}`, status })) as any
+          },
+          unitProgresses: [],
+          status: 'Failed'
+        }
+      }
+    })
+    const unitStatuses = Object.values(data.dataMap).map(row => row.unitStatus)
+    expect(unitStatuses).toMatchInlineSnapshot(`
+      Array [
+        "SUCCESS",
+        "SUCCESS",
+        "FAILED",
+      ]
+    `)
   })
 })
