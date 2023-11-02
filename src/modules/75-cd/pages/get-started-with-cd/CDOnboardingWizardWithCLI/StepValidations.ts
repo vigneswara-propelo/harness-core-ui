6867ec760bdef155c4b59eb3edc2a07171f1e186
devinder-harness <73119310+devinder-harness@users.noreply.github.com>
2023-11-02 13:05:56 -0700

    fix: Fixed Step Validation (#18748)

--- src/modules/75-cd/pages/get-started-with-cd/CDOnboardingWizardWithCLI/StepValidations.ts
+++ src/modules/75-cd/pages/get-started-with-cd/CDOnboardingWizardWithCLI/StepValidations.ts
@@ -21,9 +21,7 @@ function validateWhatToDeployStep(data: WhatToDeployType): boolean {
 
 function validateWhereAndHowToDeployStep(data: WhereAndHowToDeployType): boolean {
   return (
-    // (Boolean(data?.agentStatus === 'HEALTHY') &&
-    data.type?.id === DEPLOYMENT_FLOW_ENUMS.Gitops ||
-    // )
+    (Boolean(data?.agentStatus === 'HEALTHY') && data.type?.id === DEPLOYMENT_FLOW_ENUMS.Gitops) ||
     (Boolean(data?.installDelegateTried) && data.type?.id === DEPLOYMENT_FLOW_ENUMS.CDPipeline)
   )
 }
