9ad11b1bfb3e524c5b8a9bac530ddd04e0f97380
srdjan-harness <69592850+srdjan-harness@users.noreply.github.com>
2023-11-07 17:13:06 +0100

    fix: set undefined to prevent usage while closing connection (#18855)

--- src/modules/10-common/components/IDBContext/IDBContext.tsx
+++ src/modules/10-common/components/IDBContext/IDBContext.tsx
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
 import React, { useReducer, useRef } from 'react'
 import { deleteDB, IDBPDatabase, openDB } from 'idb'
 import SessionToken from 'framework/utils/SessionToken'
@@ -53,6 +60,7 @@ export function IDBProvider(props: IDBProviderProps): React.ReactElement {
   const cleanUpDBRefs = (): void => {
     if (idbHandleRef.current) {
       idbHandleRef.current.close()
+      idbHandleRef.current = undefined
     }
   }
 
