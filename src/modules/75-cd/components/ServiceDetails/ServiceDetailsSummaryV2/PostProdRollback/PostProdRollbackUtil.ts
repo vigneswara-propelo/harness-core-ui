d585d473d300ebd7c7583c0e3e6d059acb265873
Sagar-Rajput7 <sagar.rajput@harness.io>
2023-11-15 22:58:23 +0530

    feat: [CDS-70333]: Code cleanup for old post-prod rollback implementation (#18936)
    * feat: [CDS-70333]: Code cleanup for old postprod rollback implementation
    
    * feat: [CDS-70333]: Code cleanup - removed css

--- src/modules/75-cd/components/ServiceDetails/ServiceDetailsSummaryV2/PostProdRollback/PostProdRollbackUtil.ts
+++ src/modules/75-cd/components/ServiceDetails/ServiceDetailsSummaryV2/PostProdRollback/PostProdRollbackUtil.ts
@@ -5,47 +5,7 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-import type { Cell, CellValue, ColumnInstance, Renderer, Row, TableInstance } from 'react-table'
-import type { InstanceGroupedByChartVersion, ServiceDefinition } from 'services/cd-ng'
-
-export type PostProdRollbackStatusType = InstanceGroupedByChartVersion['rollbackStatus']
-export interface PostProdTableData {
-  artifact?: string
-  envId?: string
-  envName?: string
-  infrastructureId?: string
-  infrastructureName?: string
-  clusterId?: string
-  instanceCount?: number
-  lastDeployedAt?: number
-  showInfra?: boolean
-  showEnv?: boolean
-  showArtifact?: boolean
-  pipelineId?: string
-  pipelineName?: string
-  planexecutionId?: string
-  stageExecutionId?: string
-  stageId?: string
-  rollbackStatus?: PostProdRollbackStatusType
-  infrastructureMappingId?: string
-  instanceKey?: string
-}
-
-export interface PostProdRollbackListTableProps {
-  data: PostProdTableData[]
-  isEnvGroup: boolean
-  setDrawerOpen: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-export type CellTypeWithActions<D extends Record<string, any>, V = any> = TableInstance<D> & {
-  column: ColumnInstance<D>
-  row: Row<D>
-  cell: Cell<D, V>
-  value: CellValue<V>
-}
-
-export type CellType = Renderer<CellTypeWithActions<PostProdTableData>>
-export type CellProps = CellTypeWithActions<PostProdTableData>
+import type { ServiceDefinition } from 'services/cd-ng'
 
 export const supportedDeploymentTypesForPostProdRollback: ServiceDefinition['type'][] = [
   'Kubernetes',
@@ -55,49 +15,3 @@ export const supportedDeploymentTypesForPostProdRollback: ServiceDefinition['typ
   'Elastigroup',
   'NativeHelm'
 ]
-
-export const columnWidth = {
-  envs: {
-    haveEnvGroup: '13%',
-    noEnvGroup: '0%'
-  },
-  infras: {
-    haveEnvGroup: '15%',
-    noEnvGroup: '18%'
-  },
-  artifacts: {
-    haveEnvGroup: '20%',
-    noEnvGroup: '25%'
-  },
-  instancesCount: {
-    haveEnvGroup: '10%',
-    noEnvGroup: '10%'
-  },
-  execution: {
-    haveEnvGroup: '17%',
-    noEnvGroup: '20%'
-  },
-  status: {
-    haveEnvGroup: '12%',
-    noEnvGroup: '15%'
-  },
-  lastDeployedTime: {
-    haveEnvGroup: '13%',
-    noEnvGroup: '12%'
-  }
-}
-
-export const getRollbackStatusFromResponse = (
-  status: PostProdRollbackStatusType
-): PostProdRollbackStatusType | undefined => {
-  switch (status) {
-    case 'NOT_STARTED':
-      return 'NOT_STARTED'
-    case 'FAILURE':
-    case 'STARTED':
-    case 'SUCCESS':
-      return 'STARTED'
-    default:
-      return undefined
-  }
-}
