95abdbad5efe334f0bbc7eb81301b34f13e474ac
Chetan Vekariya <chetan.vekariya@harness.io>
2023-12-12 15:22:47 +0530

    fix: service overrides input validation (#19374)

--- src/modules/75-cd/components/PipelineSteps/DeployEnvironmentEntityStep/DeployEnvironmentEntityStep.tsx
+++ src/modules/75-cd/components/PipelineSteps/DeployEnvironmentEntityStep/DeployEnvironmentEntityStep.tsx
@@ -7,16 +7,14 @@
 
 import React from 'react'
 import { Formik, FormikErrors } from 'formik'
-import { get, isArray, isEmpty, isNil, noop, set } from 'lodash-es'
+import { isArray, isEmpty, isNil, noop, set } from 'lodash-es'
 import { IconName, MultiTypeInputType, getMultiTypeFromValue } from '@harness/uicore'
 
 import produce from 'immer'
-import { EnvironmentYamlV2 } from 'services/cd-ng'
 import { isValueRuntimeInput } from '@modules/10-common/utils/utils'
 import { Step, StepProps, StepViewType, ValidateInputSetProps } from '@pipeline/components/AbstractSteps/Step'
 import { StepType } from '@pipeline/components/PipelineSteps/PipelineStepInterface'
 import { isTemplatizedView } from '@pipeline/utils/stepUtils'
-import { AllNGVariables } from '@modules/70-pipeline/utils/types'
 import DeployEnvironmentEntityWidget from './DeployEnvironmentEntityWidget'
 import type {
   DeployEnvironmentEntityConfig,
@@ -152,44 +150,32 @@ export class DeployEnvironmentEntityStep extends Step<DeployEnvironmentEntityCon
       set(errors, 'environment.useFromStage.stage', getString?.('cd.pipelineSteps.environmentTab.useFromStageRequired'))
     }
 
-    data?.environment?.serviceOverrideInputs?.variables?.forEach((variable: AllNGVariables, index: number) => {
-      const currentVariableTemplate = get(template, `environment.serviceOverrideInputs.variables[${index}].value`, '')
-      if (
-        isRequired &&
-        ((isEmpty(variable.value) && variable.type !== 'Number') ||
-          (variable.type === 'Number' && (typeof variable.value !== 'number' || isNaN(variable.value)))) &&
-        getMultiTypeFromValue(currentVariableTemplate) === MultiTypeInputType.RUNTIME
-      ) {
-        set(
-          errors,
-          `serviceOverrideInputs.variables.[${index}].value`,
-          getString?.('fieldRequired', { field: variable.name })
-        )
-      }
-    })
+    /**
+     * @TODO This validation was correct if "variable.required" was part of if condition.
+     * We commented this part of now because required support for varible overrides is still not there in Edit and Runtime view
+     * Required support for variable is there for Edit view and Backend devs are fixing issue in Runtime view. (Reach out to @Naidu)
+     * So, considering this required support is there for variable creation, required support should be there for variable overrides as well.
+     */
 
-    if (!(errors as EnvironmentYamlV2)?.serviceOverrideInputs?.variables?.length) {
-      delete (errors as EnvironmentYamlV2)?.serviceOverrideInputs
-    }
-    data?.environment?.environmentInputs?.variables?.forEach((variable: AllNGVariables, index: number) => {
-      const currentVariableTemplate = get(template, `environment.environmentInputs.variables[${index}].value`, '')
-      if (
-        isRequired &&
-        ((isEmpty(variable.value) && variable.type !== 'Number') ||
-          (variable.type === 'Number' && (typeof variable.value !== 'number' || isNaN(variable.value)))) &&
-        getMultiTypeFromValue(currentVariableTemplate) === MultiTypeInputType.RUNTIME
-      ) {
-        set(
-          errors,
-          `environmentInputs.variables.[${index}].value`,
-          getString?.('fieldRequired', { field: variable.name })
-        )
-      }
-    })
-
-    if (!(errors as EnvironmentYamlV2)?.environmentInputs?.variables?.length) {
-      delete (errors as EnvironmentYamlV2)?.environmentInputs
-    }
+    // data?.environment?.serviceOverrideInputs?.variables?.forEach((variable: AllNGVariables, index: number) => {
+    //   const currentVariableTemplate = get(template, `environment.serviceOverrideInputs.variables[${index}].value`, '')
+    //   if (
+    //     isRequired &&
+    //     variable.required &&
+    //     ((isEmpty(variable.value) && variable.type !== 'Number') ||
+    //       (variable.type === 'Number' && (typeof variable.value !== 'number' || isNaN(variable.value)))) &&
+    //     getMultiTypeFromValue(currentVariableTemplate) === MultiTypeInputType.RUNTIME
+    //   ) {
+    //     set(
+    //       errors,
+    //       `serviceOverrideInputs.variables.[${index}].value`,
+    //       getString?.('fieldRequired', { field: variable.name })
+    //     )
+    //   }
+    // })
+    // if (!(errors as EnvironmentYamlV2)?.serviceOverrideInputs?.variables?.length) {
+    //   delete (errors as EnvironmentYamlV2)?.serviceOverrideInputs
+    // }
 
     return errors
   }
